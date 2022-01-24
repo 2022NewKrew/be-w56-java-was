2a2363d706d8d700ba42208f1d86c13a47f1e42f
gallix.kim <gallix.kim@kakaocorp.com>
2022-01-24 19:00:51 +0900

    RequestHandler 사용하지 않는 메소드 삭제

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -61,17 +61,6 @@ public class RequestHandler extends Thread {
         dos.writeBytes(responseHeader.getHeaders() + StringUtils.CR + StringUtils.LF);
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
-
     private void responseBody(DataOutputStream dos, byte[] body) {
         try {
             dos.write(body, 0, body.length);
