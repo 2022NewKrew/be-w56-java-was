3b2ea852d509cb3481e5fbc700e7784edefb05a5
chris.crunky <chris.crunky@kakaocorp.com>
2022-01-24 18:18:20 +0900

    step1-1 필수 기능 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,11 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,20 +25,46 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String line = br.readLine();
+            if (line == null) {
+                return;
+            }
+
+            log.debug("REQ: {}", line);
+            String[] tokens = line.split(" ");
+            Map<String, String> requestHeaders = new HashMap<>() {{
+                put("Method", tokens[0]);
+                put("Path", tokens[1]);
+                put("Protocol", tokens[2]);
+            }};
+
+            line = br.readLine();
+            while (line != null && !line.equals("")) {
+                String[] header = line.split(": ");
+                if (header[0].equals("Accept")) {
+                    requestHeaders.put(header[0], header[1]);
+                }
+                log.debug("     {}", line);
+                line = br.readLine();
+            }
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
+            byte[] body = Files.readAllBytes(new File("./webapp" + tokens[1]).toPath());
+            requestHeaders.put("Content-Length: ", String.valueOf(body.length));
+
+            response200Header(dos, requestHeaders);
             responseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+    private void response200Header(DataOutputStream dos, Map requestHeaders) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("Content-Length: " + requestHeaders.get("Content-Length") + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
             log.error(e.getMessage());
