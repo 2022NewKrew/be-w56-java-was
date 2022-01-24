4d2f9a7b744565bc19f7feeef433e8378257f2af
Jungyu Choi <devwithpug@kakao.com>
2022-01-24 16:28:09 +0900

    refactor: HttpStatus enum toString 리팩터링

--- src/main/java/webserver/http/HttpStatus.java
+++ src/main/java/webserver/http/HttpStatus.java
@@ -3,32 +3,33 @@ package webserver.http;
 public enum HttpStatus {
 
     // 200
-    OK(200, "200 OK"),
-    CREATED(201, "201 Created"),
+    OK(200, "OK"),
+    CREATED(201, "Created"),
 
     // 300
-    FOUND(302, "302 Found"),
+    FOUND(302, "Found"),
 
     // 400
-    BAD_REQUEST(400, "400 Bad Request"),
-    UNAUTHORIZED(401, "401 Unauthorized"),
-    FORBIDDEN(403, "403 Forbidden"),
-    NOT_FOUND(404, "404 Not Found"),
-    METHOD_NOT_ALLOWED(405, "405 Method Not Allowed"),
+    BAD_REQUEST(400, "Bad Request"),
+    UNAUTHORIZED(401, "Unauthorized"),
+    FORBIDDEN(403, "Forbidden"),
+    NOT_FOUND(404, "Not Found"),
+    METHOD_NOT_ALLOWED(405, "Method Not Allowed"),
 
     // 500
-    INTERNAL_SERVER_ERROR(500, "500 Internal Server Error");
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error");
 
 
-    final int code;
-    final String status;
+    private final int code;
+    private final String status;
 
     HttpStatus(int code, String status) {
         this.code = code;
         this.status = status;
     }
 
-    public String getStatus() {
-        return status;
+    @Override
+    public String toString() {
+        return code + " " + status;
     }
 }
