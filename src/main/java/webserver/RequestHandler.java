28cdf51ce74a00d9928b3d1a4b14927df6b1b23f
aiden.jang <aiden.jang@kakaocorp.com>
2022-01-24 16:16:38 +0900

    Start project

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -20,7 +20,7 @@ public class RequestHandler extends Thread {
 
     public void run() {
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
-                connection.getPort());
+                  connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
@@ -28,6 +28,7 @@ public class RequestHandler extends Thread {
             byte[] body = "Hello World".getBytes();
             response200Header(dos, body.length);
             responseBody(dos, body);
+             this.connection.close();
         } catch (IOException e) {
             log.error(e.getMessage());
         }
