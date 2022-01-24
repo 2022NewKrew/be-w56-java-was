430405f0031eb58cbb8e90aa4da86958ffcb96b0
JuyeoungJun <wjfhs01@gmail.com>
2022-01-24 14:54:39 +0900

    feat: Request Header 출력 및 header를 map에 저장하고 반환해주는 메소드 추가

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,13 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Socket;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.util.Map;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -23,9 +23,16 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            String line = bufferedReader.readLine();
+            log.debug("request line : {}", line);
+            String path = HttpRequestUtils.getPathFromRequestLine(line);
+
+            Map<String, String> header = HttpRequestUtils.readHeader(bufferedReader);
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            File file = new File("./webapp" + path);
+            byte[] body = Files.readAllBytes(file.toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
