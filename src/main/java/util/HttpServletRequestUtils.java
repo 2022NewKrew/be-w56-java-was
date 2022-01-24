9b26eb2b4527a4d6102bde5c7c616ad17f59cc91
HYEONSEOK1 <gustir929@gmail.com>
2022-01-24 14:43:33 +0900

    - feat : header에 따라서 contextType 불러오도록 수정

--- src/main/java/util/HttpServletRequestUtils.java
+++ src/main/java/util/HttpServletRequestUtils.java
@@ -1,12 +1,32 @@
 package util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import webserver.RequestHandler;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class HttpServletRequestUtils {
 
+    private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
     public RequestInfo parseRequestLine(String requestLine) {
         String[] tokens = requestLine.split(" ");
         RequestInfo requestInfo = new RequestInfo(tokens[0],tokens[1],tokens[2]);
         return requestInfo;
+    }
+    public Map<String,String> readHeader(BufferedReader br) throws IOException {
+        Map<String,String> headerMap = new HashMap<>();
+        String line;
+        while (!(line = br.readLine()).equals("")) {
+            String[] split = line.split(":");
+            headerMap.put(split[0].trim(),split[1].trim());
+            log.debug("header = {}", line);
+        }
+        return headerMap;
     }
     public class RequestInfo{
         private String method;
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,6 +3,7 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,31 +23,32 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            String contextType = "";
 
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
             HttpServletRequestUtils httpServletRequestUtils = new HttpServletRequestUtils();
             String line = br.readLine();
             HttpServletRequestUtils.RequestInfo requestInfo = httpServletRequestUtils.parseRequestLine(line);
             log.debug("Request line = {}", line);
 
-            while (!line.equals("")) {
-                line = br.readLine();
-                log.debug("header = {}", line);
+            Map<String, String> headerMap = httpServletRequestUtils.readHeader(br);
+            if(headerMap.containsKey("Accept")){
+                contextType = headerMap.get("Accept").split(",")[0];
             }
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
             byte[] body = Files.readAllBytes(new File("./webapp" + requestInfo.getUrl()).toPath());
-            response200Header(dos, body.length);
+            response200Header(contextType, dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+    private void response200Header(String contextType, DataOutputStream dos, int lengthOfBodyContent) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Type: "+contextType+";charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
