f535cbd4ae40ce912f90c8ce9fffb14b19a84222
reynold-yoon <reynold.yoon@kakaocorp.com>
2022-01-24 18:25:50 +0900

    웹 서버 구현 1단계
    Http request header 읽어서 path 분리하기 구현
    index.html 파일 전달 기능 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,9 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.util.Objects;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,13 +23,33 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            // log 출력
+            String requestLine = br.readLine();
+            log.debug("request line : {}", requestLine);
+//            String line = br.readLine();
+//            while (!(line).equals("")) {
+//                line = br.readLine();
+//                log.debug("header : {}", line);
+//            }
+
             DataOutputStream dos = new DataOutputStream(out);
+            String url = splitRequestLine(requestLine);
             byte[] body = "Hello World".getBytes();
+            if (!Objects.equals(url, "/")) {
+                body = Files.readAllBytes(new File("./webapp" + url).toPath());
+            }
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private String splitRequestLine(String requestLine) {
+        String[] tokens = requestLine.split(" ");
+        return tokens[1];
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
