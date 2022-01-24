da3462afcd0fe6fbfb22714406f513b5135600d4
Heejoo Shin <xenos96111@gmail.com>
2022-01-24 18:37:52 +0900

    feat: 웹 서버 1단계 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,11 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Socket;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.file.Files;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -24,19 +22,39 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String message = br.readLine();
+            byte[] body = new byte[0];
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            String targetResource = null;
+
+            while (message != null && !"".equals(message)) {
+                String[] tokens = message.split(" ");
+
+                if (tokens[0].equals("GET")) {
+                    targetResource = tokens[1];
+                    body = getBody(targetResource);
+                }
+                if (tokens[0].equals("Accept:")) {
+                    String contentType = tokens[1].split(",")[0];
+                    response200Header(dos, body.length, contentType);
+                    responseBody(dos, body);
+                    log.info("GET " + targetResource + " 200");
+                }
+                message = br.readLine();
+            }
+
         } catch (IOException e) {
             log.error(e.getMessage());
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+
+    private void response200Header(DataOutputStream dos, int lengthOfBodyContent, String contentType) {
+        String contentTypeMessage = "Content-Type: " + contentType + ";charset=utf-8\r\n";
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes(contentTypeMessage);
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
@@ -52,4 +70,15 @@ public class RequestHandler extends Thread {
             log.error(e.getMessage());
         }
     }
+
+    private byte[] getBody(String targetResource) {
+        byte[] body;
+        try {
+            body = Files.readAllBytes(new File("./webapp" + targetResource).toPath());
+        } catch (IOException e) {
+            body = "Hello World".getBytes();
+        }
+        return body;
+    }
+
 }
