4b0e3517b6b70609433bd2ea5ed4e8a70cd5326f
Hongjeongmin <wjdals0471@naver.com>
2022-01-24 11:52:32 +0900

    FEAT : BufferedReader로 HTTP 모든값 불러오기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -22,6 +22,7 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+
             MyRequest myRequest = new MyRequest(in);
 
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
