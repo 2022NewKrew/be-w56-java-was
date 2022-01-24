10a6525526592ae1e4bff0ef3bcd5484eb8cf882
kcm0147 <kcm0147@hanmail.net>
2022-01-24 13:27:26 +0900

    feat: url parsing
    - header를 parsing하여 한줄씩 출력
    - url을 parsing하여 정적페이지를 로딩할수있도록 기능 추가

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,8 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,7 +10,8 @@ import org.slf4j.LoggerFactory;
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
-    private Socket connection;
+    private final Socket connection;
+    private final String BLANK = "";
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
@@ -23,9 +22,23 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            String line = br.readLine();
+
+            if (line == null) {
+                return;
+            }
+            String[] tokens = line.split(" ");
+            String url = tokens[1];
+            log.debug("url : {}", url);
+            log.debug("request line : {}", line);
+            while (!line.equals(BLANK)) {
+                log.debug("header : {}", line);
+                line = br.readLine();
+            }
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
