1a0930d02ec90965d56f98a628eb3984d4245500
Daecheol Shin <60842206+cheol7826@users.noreply.github.com>
2022-01-24 22:07:18 +0900

    웹서버 Step1 구현 (#47)
    * 웹서버 Step1 구현
    
    - Request URL Parsing 기능 구현
    - 헤더 정보 로그로 출력하도록 구현
    - Css, Js 요청 동작하도록 변경
    
    * url 파싱 테스트 추가
    
    - parseUrl 관련 테스트 추가
    - 파싱 로직 오류 수정
    -> /index.html 요청시 //index.html로 파싱되는 부분 수정
    
    * README.md 내용 추가
    
    Co-authored-by: iron.shin <iron.shin@kakaocorp.com>

--- src/main/java/util/HttpRequestUtils.java
+++ src/main/java/util/HttpRequestUtils.java
@@ -8,6 +8,17 @@ import com.google.common.base.Strings;
 import com.google.common.collect.Maps;
 
 public class HttpRequestUtils {
+    public static String parseUrl(String requestLine){
+        String[] requestLineTokens = requestLine.split(" ");
+        String url = requestLineTokens[1];
+
+        if(url.equals("/")){
+            return "/index.html";
+        }
+
+        return url;
+    }
+
     /**
      * @param queryString은
      *            URL에서 ? 이후에 전달되는 field1=value1&field2=value2 형식임
