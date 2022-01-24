1d9f220dba8c2fbd9b72a4cd1760c44f773b9082
konempty <44064530+konempty@users.noreply.github.com>
2022-01-24 22:06:29 +0900

    Step1-1 요구사항 충족 (#5)
    - 들어오는 입력을 Request객체로 매핑
    - request의 모든 header 출력
    - request에서 path를 사용하여 리소스 반환
    - 없는 리소스 요청시에 404 response 반환
    
    Co-authored-by: konempty <konempty@github.com>

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,18 +1,21 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Socket;
-
+import model.Request;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
+import util.HttpRequestUtils.Pair;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.util.List;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
+    private static final byte[] NOT_FOUNT_MESSAGE = "없는 페이지 입니다.".getBytes();
 
-    private Socket connection;
+    private final Socket connection;
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
@@ -23,10 +26,18 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            Request request = HttpRequestUtils.createRequest(in);
+            printHeaders(request.getHeader());
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
+            File file = new File("./webapp" + request.getPath());
+            byte[] body;
+            if (file.exists()) {
+                body = Files.readAllBytes(file.toPath());
+                response200Header(dos, body.length);
+            } else {
+                body = NOT_FOUNT_MESSAGE;
+                response404Header(dos);
+            }
             responseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
@@ -44,12 +55,29 @@ public class RequestHandler extends Thread {
         }
     }
 
+    private void response404Header(DataOutputStream dos) {
+        try {
+            dos.writeBytes("HTTP/1.1 404 Not Found \r\n");
+            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Length: " + NOT_FOUNT_MESSAGE.length + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
     private void responseBody(DataOutputStream dos, byte[] body) {
         try {
             dos.write(body, 0, body.length);
             dos.flush();
         } catch (IOException e) {
             log.error(e.getMessage());
+        }
+    }
+
+    private void printHeaders(List<Pair> header) {
+        for (Pair pair : header) {
+            log.info("Header : [{} : {}]", pair.getKey(), pair.getValue());
         }
     }
 }
