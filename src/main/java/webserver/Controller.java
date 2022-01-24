856a424574433e6751e7b05ce365236dc39f38dc
nike0717 <63907002+nike0717@users.noreply.github.com>
2022-01-24 22:06:56 +0900

    웹 서버 1단계 구현 (#29)
    * 정적 파일 응답 서버 구현
    
    * 습득 지식 정리 내용 작성(README)
    
    Co-authored-by: kenny.park <kenny.park@kakaocorp.com>

---
+++ src/main/java/webserver/Controller.java
@@ -0,0 +1,21 @@
+package webserver;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class Controller {
+    private Request request;
+    private Response response;
+
+    public Controller(Request request, Response response){
+        this.request = request;
+        this.response = response;
+    }
+
+    public void start() throws IOException {
+        byte[] body = Files.readAllBytes(new File("./webapp" + request.getUri()).toPath());
+        response.writeHeader(body.length, 200);
+        response.writeBody(body);
+    }
+}
---
+++ src/main/java/webserver/Response.java
@@ -0,0 +1,26 @@
+package webserver;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.io.*;
+
+public class Response {
+    private static final Logger log = LoggerFactory.getLogger(Response.class);
+    private DataOutputStream dos;
+
+    public Response(OutputStream out) throws IOException {
+        this.dos = new DataOutputStream(out);
+    }
+
+    public void writeHeader(int lengthOfBodyContent, int status) throws IOException {
+        dos.writeBytes("HTTP/1.1 "+Integer.toString(status)+" OK \r\n");
+        //dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+        dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+        dos.writeBytes("\r\n");
+    }
+
+    public void writeBody(byte[] body) throws IOException {
+        dos.write(body, 0, body.length);
+        dos.flush();
+    }
+}
