0f41970a0977a97e33a35658fe3d86d97378a321
HYEONSEOK1 <37043329+HYEONSEOK1@users.noreply.github.com>
2022-01-24 22:06:25 +0900

    웹 서버 구현 1단계 (#2)
    * - feat : servlet역할을 하는 httprequestutill 생성 후, url 에 매핑해주는 로직 추가
    
    * - feat : header에 따라서 contextType 불러오도록 수정
    
    * - refactor : utill 함수들 static으로 변경
    
    * -feat : lombok 추가 후, HttpServletRequestUtils test 생성
    
    * - readme 업데이트

---
+++ src/main/java/util/HttpServletRequestUtils.java
@@ -0,0 +1,44 @@
+package util;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import webserver.RequestHandler;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class HttpServletRequestUtils {
+
+    private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
+
+    public static RequestInfo parseRequestLine(String requestLine) {
+        String[] tokens = requestLine.split(" ");
+        RequestInfo requestInfo = new RequestInfo(tokens[0],tokens[1],tokens[2]);
+        return requestInfo;
+    }
+    public static Map<String,String> readHeader(BufferedReader br) throws IOException {
+        Map<String,String> headerMap = new HashMap<>();
+        String line;
+        while (!(line = br.readLine()).equals("")) {
+            String[] split = line.split(":",2);
+            headerMap.put(split[0].trim(),split[1].trim());
+            log.debug("header = {}", line);
+        }
+        return headerMap;
+    }
+    @Getter
+    @EqualsAndHashCode
+    @AllArgsConstructor
+    public static class RequestInfo{
+        private String method;
+        private String url;
+        private String protocol;
+    }
+}
