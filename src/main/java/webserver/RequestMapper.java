1dfa2bdc196783f4b673076769a882f73bbdf197
Jungyu Choi <devwithpug@kakao.com>
2022-01-24 18:04:29 +0900

    feat: MIME enum 추가 & 정적 리소스 매핑 로직 리팩터링

--- src/main/java/webserver/RequestMapper.java
+++ src/main/java/webserver/RequestMapper.java
@@ -1,17 +1,26 @@
 package webserver;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import webserver.http.HttpStatus;
+import webserver.http.MIME;
 import webserver.http.MyHttpRequest;
 import webserver.http.MyHttpResponse;
 
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.file.Files;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class RequestMapper {
 
+    private static final Logger log = LoggerFactory.getLogger(RequestMapper.class);
     private static final Map<String, RequestMapping> requestMap;
+    private static final List<String> STATIC_RESOURCE_DIR = List.of("/js", "/css", "/fonts", "images", "/favicon.ico");
 
     static {
         requestMap = new HashMap<>();
@@ -24,11 +33,36 @@ public class RequestMapper {
         String path = in.uri().getPath();
         DataOutputStream dos = new DataOutputStream(out);
 
+        boolean isRequestStaticResource = STATIC_RESOURCE_DIR.stream()
+                .anyMatch(path::startsWith);
+
+        if (isRequestStaticResource) {
+            responseStaticResource(dos, path);
+            return;
+        }
+
         if (!requestMap.containsKey(path)) {
             response404NotFound(dos);
             return;
         }
         requestMap.get(path).handle(in, dos);
+    }
+
+    private static void responseStaticResource(DataOutputStream dos, String path) {
+        try {
+            byte[] body = Files.readAllBytes(new File("./webapp" + path).toPath());
+
+            MyHttpResponse response = MyHttpResponse.builder(dos)
+                    .status(HttpStatus.OK)
+                    .contentType(MIME.parse(path))
+                    .body(body)
+                    .build();
+
+            response.writeBytes();
+            response.flush();
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
     }
 
     private static void response404NotFound(DataOutputStream dos) {
@@ -61,11 +95,23 @@ public class RequestMapper {
         INDEX("/index.html") {
             @Override
             public void handle(MyHttpRequest request, DataOutputStream dos) {
-                // TODO - 구현 예정
+                try {
+                    byte[] body = Files.readAllBytes(new File("./webapp/index.html").toPath());
+
+                    MyHttpResponse response = MyHttpResponse.builder(dos)
+                            .status(HttpStatus.OK)
+                            .body(body)
+                            .build();
+
+                    response.writeBytes();
+                    response.flush();
+                } catch (IOException e) {
+                    log.error(e.getMessage());
+                }
             }
         };
 
-        final String path;
+        private final String path;
 
         RequestMapping(String path) {
             this.path = path;
