7e805188892bb6acaf6294b9bc80642208220d1b
Kimgeunwook <wook961206@naver.com>
2022-01-24 17:38:05 +0900

    Feat: step1 완료

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # be-w56-java-was
-56주차 간단 웹 서버 구현
+5,6주차 간단 웹 서버 구현
+
+# step1 
+- 알게된점
+    1. 제일 처음 html을 가져오고 해당 파일 안에있는 css,js정보를 통해 추가 request를 요청한다는점
+
+- 문제 해결
+    1. 처음 index.html을 요청하니 css가 적용이 되지 않았다. -> response에 context-type을 text/html로 고정되어 있던부분을 수정해줬다.
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,14 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Socket;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.RequestUtills;
+import util.ResponseUtils;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.util.Map;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -23,31 +24,19 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String requestUrl = RequestUtills.readUrlPath(br);
+
+            Map<String, String> headerMap = RequestUtills.readHeader(br);
+            String respContextType = "";
+            if (headerMap.containsKey("Accept")) {
+                respContextType = headerMap.get("Accept").split(",")[0];
+            }
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
-
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
-
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
+            byte[] body = Files.readAllBytes(new File("./webapp" + requestUrl).toPath());
+            ResponseUtils.response200Header(respContextType, dos, body.length);
+            ResponseUtils.responseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
