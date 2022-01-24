3c2191862457b4c9e84b27e972f3c5bac228171f
jonghoon ok <jonghoone94@gmail.com>
2022-01-24 23:44:09 +0900

    1단계 : 정적인 html 파일 응답

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,11 +1,11 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.util.Map;
 
+import model.RequestHeader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,20 +23,42 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            String requestLine = br.readLine();
+            RequestHeader requestHeader = new RequestHeader();
+
+            if(requestLine != null) {
+                log.info("request line : {}", requestLine);
+                setHeader(requestHeader, requestLine, br);
+            }
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
+            byte[] body = Files.readAllBytes(new File("./webapp" + requestHeader.getPath()).toPath());
+            response200Header(dos, body.length, requestHeader);
             responseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+    private void setHeader(RequestHeader requestHeader, String requestLine, BufferedReader br) throws IOException {
+        String[] tokens = requestLine.split(" ");
+        requestHeader.setMethod(tokens[0]);
+        requestHeader.setPath(tokens[1]);
+
+        String nextLine;
+        Map<String, String> headers = requestHeader.getHeaders();
+        while (!(nextLine = br.readLine()).equals("")){
+            log.info("header : {}", nextLine);
+            String[] header = nextLine.split(": ");
+            headers.put(header[0], header[1]);
+        }
+    }
+
+    private void response200Header(DataOutputStream dos, int lengthOfBodyContent, RequestHeader requestHeader) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Type: " + requestHeader.getContentType() + ";charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
