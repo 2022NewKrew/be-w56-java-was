128b59f79af22331ad3f291e6ee7fa06a4576ae1
Redbin <30316976+Hongjeongmin@users.noreply.github.com>
2022-01-24 22:07:21 +0900

    Step1 (#49)
    * FEAT : BufferedReader로 HTTP 모든값 불러오기
    
    * DOCS : HTTP 학습
    
    쿨라임의 HTTP/1.1, HTTP/2 QUIC
    
    * FEAT : request 요청 처리 로직 구현
    
    * FEAT : MyRequest 상세 구현
    
    getPathVariable 추가 : url params 값 적용
    
    그외 버전 정보, accept 정보 추가.
    
    * REFACTOR : 파일 위치 이동
    
    utll 패키지로 MyRequest 이동
    
    * REFACTOR : MyRequest 파일 이동

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,12 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Socket;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.MyRequest;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.file.Files;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -23,9 +22,11 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+            MyRequest myRequest = new MyRequest(in);
+
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            byte[] body = Files.readAllBytes(new File("./webapp" + myRequest.getRequestUrl()).toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
