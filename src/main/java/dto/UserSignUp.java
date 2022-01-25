f29904fba95b501b69a5fb4778dbea313e8c98b5
chen.kim <fromecha@gmail.com>
2022-01-25 14:43:23 +0900

    [feat] 유저 회원가입 dto 생성

---
+++ src/main/java/dto/UserSignUp.java
@@ -0,0 +1,32 @@
+package dto;
+
+public class UserSignUp {
+
+    private final String userId;
+    private final String password;
+    private final String name;
+    private final String email;
+
+    public UserSignUp(String userId, String password, String name, String email) {
+        this.userId = userId;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
