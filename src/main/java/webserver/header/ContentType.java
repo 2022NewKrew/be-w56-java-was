abf587ca81616410b3c0e6aff213f69c6f7c6cb8
edward.baek <edward.baek@kakaocorp.com>
2022-01-25 17:19:07 +0900

    feat : HttpMethod과 ContentType을 위한 열거형 생성

---
+++ src/main/java/webserver/header/ContentType.java
@@ -0,0 +1,38 @@
+package webserver.header;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public enum ContentType {
+    HTML("text/html", "html"),
+    CSS("text/css", "css"),
+    JS("application/javascript", "js"),
+    JSON("application/json", "json"),
+    ICO("image/x-icon", "ico");
+
+    private final String type;
+    private final String extension;
+
+    private static final Map<String, ContentType> descriptions = Collections
+            .unmodifiableMap(Stream.of(values())
+                    .collect(Collectors.toMap(ContentType::getExtension, Function.identity())));
+
+    ContentType(String type, String extension) {
+        this.type = type;
+        this.extension = extension;
+    }
+
+    public static ContentType matching(String extension) {
+        return Optional.ofNullable(descriptions.get(extension)).orElse(HTML);
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getExtension() { return extension; }
+}
