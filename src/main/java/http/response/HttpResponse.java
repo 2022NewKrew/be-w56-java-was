56be6f032ec9d7886cca2da910562ea648874a4a
= <=>
2022-01-26 00:46:40 +0900

    HttpResponseHeader가 statusLine과 headers 필드를 포함하도록 수정

--- src/main/java/http/response/HttpResponse.java
+++ src/main/java/http/response/HttpResponse.java
@@ -4,19 +4,16 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 
 public class HttpResponse {
-    private final HttpResponseStatusLine statusLine;
     private final HttpResponseHeader header;
     private final HttpResponseBody body;
 
-    public HttpResponse(HttpResponseStatusLine statusLine, HttpResponseHeader header, HttpResponseBody body) {
-        this.statusLine = statusLine;
+    public HttpResponse(HttpResponseHeader header, HttpResponseBody body) {
         this.header = header;
         this.body = body;
     }
 
     public void writeToDataOutputStream(DataOutputStream dos) throws IOException {
         try {
-            statusLine.writeToDataOutputStream(dos);
             header.writeToDataOutputStream(dos);
             body.writeToDataOutputStream(dos);
         } catch (IOException e) {
