44f829cbcab30f90612ffb14fc309f827bf93030
= <=>
2022-01-25 09:50:17 +0900

    try-with-resources 문으로 자동 close되게 수정

---
+++ src/main/java/request/HttpRequestLine.java
@@ -0,0 +1,15 @@
+package request;
+
+public class HttpRequestLine {
+    private final String method;
+    private final String url;
+    private final String httpVersion;
+
+    public HttpRequestLine(String method, String url, String httpVersion) {
+        this.method = method;
+        this.url = url;
+        this.httpVersion = httpVersion;
+    }
+
+    public String getUrl() { return url; }
+}
