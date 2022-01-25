8debf9c088087fd6c3ed1cda507e663c035f7f63
= <=>
2022-01-26 00:40:13 +0900

    HttpResponseStatusLine 추가 (response header에서 분리)

--- src/main/java/http/response/HttpResponse.java
+++ src/main/java/http/response/HttpResponse.java
@@ -4,16 +4,19 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 
 public class HttpResponse {
+    private final HttpResponseStatusLine statusLine;
     private final HttpResponseHeader header;
     private final HttpResponseBody body;
 
-    public HttpResponse(HttpResponseHeader header, HttpResponseBody body) {
+    public HttpResponse(HttpResponseStatusLine statusLine, HttpResponseHeader header, HttpResponseBody body) {
+        this.statusLine = statusLine;
         this.header = header;
         this.body = body;
     }
 
     public void writeToDataOutputStream(DataOutputStream dos) throws IOException {
         try {
+            statusLine.writeToDataOutputStream(dos);
             header.writeToDataOutputStream(dos);
             body.writeToDataOutputStream(dos);
         } catch (IOException e) {
