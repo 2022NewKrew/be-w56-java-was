39f420ccc7dd42c17827ee6a54a90addc4364c27
jb.jang <jb.jang@kakaocorp.com>
2022-01-25 14:46:05 +0900

    feat: UserCreateRequestDto 추가

---
+++ src/main/java/dto/UserCreateRequestDto.java
@@ -0,0 +1,14 @@
+package dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserCreateRequestDto {
+
+    private final String userId;
+    private final String password;
+    private final String name;
+    private final String email;
+}
