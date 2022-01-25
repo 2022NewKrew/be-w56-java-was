0098bcb3e3e70077ed668b1a82f5930cb4607030
edward.baek <edward.baek@kakaocorp.com>
2022-01-25 17:17:31 +0900

    feat : 유저 저장 서비스 dto

---
+++ src/main/java/service/dto/EnrollUserCommand.java
@@ -0,0 +1,26 @@
+package service.dto;
+
+import model.User;
+
+public class EnrollUserCommand {
+
+    private final String userId;
+    private final String password;
+    private final String name;
+    private final String email;
+
+    public EnrollUserCommand(String userId, String password, String name, String email) {
+        this.userId = userId;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+
+    public User toEntity() {
+        return new User(
+                userId,
+                password,
+                name,
+                email);
+    }
+}
