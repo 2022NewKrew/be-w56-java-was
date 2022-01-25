d9a67e60a3b971b3393e265210d84a2cd679b49d
pi.raspberry <pi.raspberry@kakaocorp.com>
2022-01-25 17:20:07 +0900

    Add unit test for HttpHeaderUntils class
    - ParameterizedTest를 적용하여 단위 테스트를 구현함.

--- src/test/java/util/HttpHeaderUtilsTest.java
+++ src/test/java/util/HttpHeaderUtilsTest.java
@@ -2,8 +2,12 @@ package util;
 
 import model.User;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.UnsupportedEncodingException;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -19,5 +23,29 @@ class HttpHeaderUtilsTest {
     void getUserInfoFromUrl() throws UnsupportedEncodingException {
         var result = HttpHeaderUtils.getUserInfoFromUrl("userId=javajigi&password=password&name=%EB%B0%95%EC%9E%AC%EC%84%B1&email=javajigi%40slipp.net");
         assertThat(result).isEqualTo(new User("javajigi", "password", "박재성", "javajigi@slipp.net"));
+    }
+
+    @Test
+    void getQuery() {
+        Optional<String> result = HttpHeaderUtils.getQuery("/create?userId=javajigi&password=password&name=%EB%B0%95%EC%9E%AC%EC%84%B1&email=javajigi%40slipp.net");
+        assertThat(result).isEqualTo(Optional.of("userId=javajigi&password=password&name=%EB%B0%95%EC%9E%AC%EC%84%B1&email=javajigi%40slipp.net"));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"/create?userId=javajigi&password=password&name=%EB%B0%95%EC%9E%AC%EC%84%B1&email=javajigi%40slipp.net,/create",
+            "/index.html,/index.html",
+            "/user/form.html,/user/form.html"})
+    void getUrl(String input, String expected) {
+        String result = HttpHeaderUtils.getUrl(input);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"/index.html,text/html",
+            "/js/jquery-2.2.0.min.js,text/javascript",
+            "/css/styles.css,text/css"})
+    void getContentTypeFromUrl(String input, String expected) {
+        String result = HttpHeaderUtils.getContentTypeFromUrl(input);
+        assertThat(result).isEqualTo(expected);
     }
 }
