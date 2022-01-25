53b59c8f5a045c832b1d165ffed9bc5db8ca17bf
= <=>
2022-01-25 12:06:25 +0900

    HttpResponseHeader 추가

--- src/main/java/response/HttpResponseHeader.java
+++ src/main/java/response/HttpResponseHeader.java
@@ -1,4 +1,24 @@
 package response;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+
 public class HttpResponseHeader {
+    private final String header;
+
+    public HttpResponseHeader(String contentType, int bodyLength) {
+        header = "HTTP/1.1 200 OK \r\n" +
+                "Content-Type: " + contentType + ";charset=utf-8\r\n" +
+                "Content-Length: " + bodyLength + "\r\n" +
+                "\r\n";
+    }
+
+    public void writeToDataOutputStream(DataOutputStream dos) {
+        try {
+            dos.writeBytes(header);
+        } catch (IOException e) {
+
+        }
+
+    }
 }
