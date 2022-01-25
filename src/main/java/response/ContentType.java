da48b98445dc45db9a38d6a18cbc7f62d2c84976
= <=>
2022-01-25 14:21:52 +0900

    response, request 클래스 http 패키지 하위로 이동

--- src/main/java/response/ContentType.java
+++
@@ -1,8 +0,0 @@
-package response;
-
-public class ContentType {
-    public static final String CSS = "text/css";
-    public static final String JS = "text/javascript";
-    public static final String HTML = "text/html";
-    public static final String DEFAULT = "application/octet-stream";
-}
