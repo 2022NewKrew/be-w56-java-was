681b9f38801381c356f46d13d02ff1fef379f58d
ato.mung <ato.mung@kakaocorp.com>
2022-01-25 14:06:14 +0900

    feat: 기초작업 추가
    main 클래스 분리
    스레드풀 적용
    포트범위 검사

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -1,34 +1,55 @@
 package webserver;
 
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Stream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class WebServer {
-    private static final Logger log = LoggerFactory.getLogger(WebServer.class);
+
+    private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 8080;
 
-    public static void main(String args[]) throws Exception {
-        int port = 0;
-        if (args == null || args.length == 0) {
-            port = DEFAULT_PORT;
-        } else {
-            port = Integer.parseInt(args[0]);
+    private final ExecutorService executorService = Executors.newFixedThreadPool(20);
+    private final int port;
+
+    public WebServer(int port) {
+        this.port = checkPort(port);
+    }
+
+    public void run() {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            logger.info("Web Server started {} port.", serverSocket.getLocalPort());
+            handle(serverSocket);
+        } catch (IOException exception) {
+            logger.error("Exception accepting connection", exception);
+        } catch (RuntimeException exception) {
+            logger.error("Unexpected error", exception);
         }
+    }
 
-        // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
-
-        try (ServerSocket listenSocket = new ServerSocket(port)) {
-            log.info("Web Application Server started {} port.", port);
-
-            // 클라이언트가 연결될때까지 대기한다.
-            Socket connection;
-            while ((connection = listenSocket.accept()) != null) {
-                RequestHandler requestHandler = new RequestHandler(connection);
-                requestHandler.start();
-            }
+    private void handle(ServerSocket listenSocket) throws IOException {
+        Socket connection;
+        while ((connection = listenSocket.accept()) != null) {
+            executorService.submit(new RequestHandler(connection));
         }
+    }
+
+    public static int defaultPortIfNull(String[] args) {
+        return Stream.of(args)
+            .findFirst()
+            .map(Integer::parseInt)
+            .orElse(WebServer.DEFAULT_PORT);
+    }
+
+    private int checkPort(int port) {
+        if (port < 1 || 65535 < port) {
+            return DEFAULT_PORT;
+        }
+        return port;
     }
 }
