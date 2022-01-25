a50dcd171aeb6a9560ed541270437037acf4fe38
Jungyu Choi <devwithpug@kakao.com>
2022-01-25 11:16:53 +0900

    feat: 회원가입 기능 구현

--- src/main/java/webserver/RequestMappingInfo.java
+++ src/main/java/webserver/RequestMappingInfo.java
@@ -1,12 +1,18 @@
 package webserver;
 
+import db.DataBase;
+import model.User;
+import util.HttpRequestUtils;
+import webserver.dto.UserCreateRequest;
 import webserver.http.HttpStatus;
 import webserver.http.MyHttpRequest;
 import webserver.http.MyHttpResponse;
 
 import java.io.DataOutputStream;
 import java.io.File;
+import java.net.URI;
 import java.nio.file.Files;
+import java.util.Map;
 
 public enum RequestMappingInfo {
 
@@ -18,6 +24,25 @@ public enum RequestMappingInfo {
             MyHttpResponse response = MyHttpResponse.builder(dos)
                     .status(HttpStatus.OK)
                     .body(body)
+                    .build();
+
+            response.writeBytes();
+            response.flush();
+        }
+    },
+    SIGN_UP("/create") {
+        @Override
+        public void handle(MyHttpRequest request, DataOutputStream dos) throws Exception {
+            URI uri = request.uri();
+            Map<String, String> parameterMap = HttpRequestUtils.parseQueryString(uri.getQuery());
+
+            UserCreateRequest userCreateRequest = UserCreateRequest.of(parameterMap);
+            User user = userCreateRequest.toEntity();
+            DataBase.addUser(user);
+
+            MyHttpResponse response = MyHttpResponse.builder(dos)
+                    .status(HttpStatus.FOUND)
+                    .header("Location", "/")
                     .build();
 
             response.writeBytes();
