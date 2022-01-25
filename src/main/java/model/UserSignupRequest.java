073f2ed6d26d5678724da25e218552f45c2c2c74
jh Lim <jhwlim25@gmail.com>
2022-01-25 16:03:26 +0900

    feat : 회원가입 요청 유효성 검사 적용

--- src/main/java/model/UserSignupRequest.java
+++ src/main/java/model/UserSignupRequest.java
@@ -1,16 +1,40 @@
 package model;
 
+import org.apache.commons.lang3.StringUtils;
+import webserver.exception.InvalidInputException;
+
 public class UserSignupRequest {
-    private String userId;
-    private String password;
-    private String name;
-    private String email;
+    private final String userId;
+    private final String password;
+    private final String name;
+    private final String email;
 
     public UserSignupRequest(String userId, String password, String name, String email) {
         this.userId = userId;
         this.password = password;
         this.name = name;
         this.email = email;
+    }
+
+    public void validate() {
+        validateBlank(userId);
+        validateBlank(password);
+        validateBlank(name);
+        validateBlank(email);
+        validateEmail(email);
+    }
+
+    private void validateBlank(String param) {
+        if (StringUtils.isBlank(param)) {
+            throw new InvalidInputException("공백을 입력하였습니다.");
+        }
+    }
+
+    private void validateEmail(String email) {
+        String regex = "^[_a-z0-9-]+(.[_a-z0-9-]+)*@(?:\\w+\\.)+\\w+$";
+        if (!email.matches(regex)) {
+            throw new InvalidInputException("이메일 형식이 잘못되었습니다.");
+        }
     }
 
     public User toUser() {
--- src/main/java/service/user/SignupService.java
+++ src/main/java/service/user/SignupService.java
@@ -6,6 +6,7 @@ import model.UserSignupRequest;
 public class SignupService {
 
     public void signup(UserSignupRequest request) {
+        request.validate();
         DataBase.addUser(request.toUser());
     }
 
