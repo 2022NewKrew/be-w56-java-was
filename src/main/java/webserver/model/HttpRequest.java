2294b2e82a8ef2d0cb557a877ec08b0c462c8295
jh Lim <jhwlim25@gmail.com>
2022-01-25 13:52:32 +0900

    feat : HttpRequest가 query string 저장하도록 수정

--- src/main/java/webserver/model/HttpRequest.java
+++ src/main/java/webserver/model/HttpRequest.java
@@ -5,26 +5,53 @@ import util.HttpRequestUtils;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HttpRequest {
     private static final int INDEX_OF_HTTP_METHOD = 0;
     private static final int INDEX_OF_URL = 1;
+    private static final int INDEX_OF_URL_PATH = 0;
+    private static final int INDEX_OF_URL_QUERY_STRING = 1;
+    private static final String queryStringRegex = "\\?";
 
     private final HttpMethod httpMethod;
     private final String url;
+    private final Map<String, String> queryStrings;
     private final HttpHeaders headers;
 
     public HttpRequest(BufferedReader br) throws IOException {
-        String[] requestLine = br.readLine().split(" ");
+        String[] requestLine = splitRequestLine(br.readLine());
         this.httpMethod = HttpMethod.parseHttpMethod(requestLine[INDEX_OF_HTTP_METHOD]);
-        this.url = requestLine[INDEX_OF_URL];
-        this.headers = new HttpHeaders();
+
+        String[] requestUrl = splitRequestUrl(requestLine[INDEX_OF_URL]);
+        this.url = requestUrl[INDEX_OF_URL_PATH];
+        this.queryStrings = requestUrl.length > 1 ? parseQueryStrings(requestUrl[INDEX_OF_URL_QUERY_STRING]) : new HashMap<>();
+
+        this.headers = parseHttpHeader(br);
+    }
+
+    private String[] splitRequestLine(String requestLine) {
+        return requestLine.split(StringUtils.SPACE);
+    }
+
+    private String[] splitRequestUrl(String requestUrl) {
+        return requestUrl.split(queryStringRegex);
+    }
+
+    private HttpHeaders parseHttpHeader(BufferedReader br) throws IOException {
+        HttpHeaders headers = new HttpHeaders();
         String line = br.readLine();
         while (!StringUtils.isEmpty(line)) {
             HttpRequestUtils.Pair pair = HttpRequestUtils.parseHeader(line);
             headers.addHeaders(pair.getKey(), pair.getKey());
             line = br.readLine();
         }
+        return headers;
+    }
+
+    private Map<String, String> parseQueryStrings(String queryStrings) {
+        return HttpRequestUtils.parseQueryString(queryStrings);
     }
 
     public HttpMethod getHttpMethod() {
