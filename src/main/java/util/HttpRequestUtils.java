03166ef6e2e2d553032e9b54e2e379113067293b
SaeGon-Heo <her84562@gmail.com>
2022-01-25 14:55:25 +0900

    문구 수정

--- src/main/java/util/HttpRequestUtils.java
+++ src/main/java/util/HttpRequestUtils.java
@@ -15,14 +15,14 @@ public class HttpRequestUtils {
     private static final String VALUE_SEPARATOR = "=";
 
     /**
-     * @apiNote  queryString은 URL에서 ? 이후에 전달되는 field1=value1&field2=value2 형식임
+     * @param queryString queryString은 URL에서 ? 이후에 전달되는 field1=value1&field2=value2 형식임
      */
     public static Map<String, String> parseQueryString(String queryString) {
         return parseValues(queryString, PARAMETER_SEPARATOR);
     }
 
     /**
-     * @apiNote 쿠키 값은 name1=value1; name2=value2 형식임
+     * @param cookies 쿠키 값은 name1=value1; name2=value2 형식임
      */
     public static Map<String, String> parseCookies(String cookies) {
         return parseValues(cookies, COOKIE_SEPARATOR);
