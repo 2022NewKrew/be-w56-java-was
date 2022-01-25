69ee7909b2baa18a7923a783eec2ee20fc58e7e1
jb.jang <jb.jang@kakaocorp.com>
2022-01-25 14:41:52 +0900

    feat: User 객체 생성시 로그 추가

--- src/main/java/db/DataBase.java
+++ src/main/java/db/DataBase.java
@@ -6,12 +6,17 @@ import java.util.Map;
 import com.google.common.collect.Maps;
 
 import model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DataBase {
     private static Map<String, User> users = Maps.newHashMap();
+    private static final Logger log = LoggerFactory.getLogger(DataBase.class);
+
 
     public static void addUser(User user) {
         users.put(user.getUserId(), user);
+        log.debug("New User Added! userId : {}, name : {}", user.getUserId(), user.getName());
     }
 
     public static User findUserById(String userId) {
