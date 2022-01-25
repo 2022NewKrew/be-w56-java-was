979f88d4743d6af0eb446b6a9239a6b62ba5ca3f
adj5672 <77478791+adj5672@users.noreply.github.com>
2022-01-25 22:06:20 +0900

    Step 2 웹 서버 만들기 (#125)
    * content-negotiation 간단 구현
    
    * HttpRequest 클래스 생성
    
    * HttpResponse & Handler Mapper 생성
    
    * RequestLine 에서 RequestParameter 추출
    
    * GET 회원가입 기능 구현
    
    * HTTP Status enum 생성
    
    Co-authored-by: danny.an <danny.an@kakaocorp.com>

---
+++ src/main/java/util/HandlerMapper.java
@@ -0,0 +1,65 @@
+package util;
+
+import model.User;
+import network.HttpMethod;
+import network.HttpRequest;
+import network.HttpStatus;
+import network.ResponseBody;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.Map;
+
+public class HandlerMapper {
+
+    private static final Logger log = LoggerFactory.getLogger(HandlerMapper.class);
+
+    public static ResponseBody requestMapping(HttpRequest httpRequest) throws IOException {
+        HttpMethod httpMethod = httpRequest.getMethod();
+        String path = httpRequest.getPath();
+        Map<String, String> queryString = httpRequest.getQueryString();
+
+        if (httpMethod.equals(HttpMethod.GET)) {
+            switch (path) {
+                case "/" : return indexView();
+                case "/user/signup" : return signupView();
+                case "/user/create" : return signup(queryString);
+                default : return defaultPath(path);
+            }
+        }
+        return null;
+    }
+
+    private static ResponseBody indexView() throws IOException {
+        byte[] body = Files.readAllBytes(new File("./webapp/index.html").toPath());
+        return new ResponseBody(HttpStatus.OK, body);
+    }
+
+    private static ResponseBody signupView() throws IOException {
+        byte[] body = Files.readAllBytes(new File("./webapp/user/form.html").toPath());
+        return new ResponseBody(HttpStatus.OK, body);
+    }
+
+    private static ResponseBody signup(Map<String, String> queryString) throws IOException {
+        String userId = URLDecoder.decode(queryString.get("userId"), StandardCharsets.UTF_8);
+        String password = URLDecoder.decode(queryString.get("password"), StandardCharsets.UTF_8);
+        String name = URLDecoder.decode(queryString.get("name"), StandardCharsets.UTF_8);
+        String email = URLDecoder.decode(queryString.get("email"), StandardCharsets.UTF_8);
+        User user = new User(userId, password, name, email);
+
+        log.debug(user.toString());
+
+        byte[] body = Files.readAllBytes(new File("./webapp/user/list.html").toPath());
+        return new ResponseBody(HttpStatus.OK, body);
+    }
+
+    private static ResponseBody defaultPath(String path) throws IOException{
+        byte[] body = Files.readAllBytes(new File("./webapp" + path).toPath());
+        return new ResponseBody(HttpStatus.OK, body);
+    }
+}
--- src/main/java/util/HttpRequestUtils.java
+++ src/main/java/util/HttpRequestUtils.java
@@ -10,12 +10,51 @@ import com.google.common.collect.Maps;
 
 public class HttpRequestUtils {
 
+    public static String contentNegotation (String accept) {
+        String mime = accept.split(",")[0];
+        if (mime.contains(";")) {
+            mime = mime.split(";")[0];
+        }
+
+        String[] mimeType = mime.split("/");
+        String type = mimeType[0];
+        String subType = mimeType[1];
+        if (type.equals("*")) {
+            return "text/plain";
+        }
+
+        if (subType.equals("*")) {
+            switch (type) {
+                case "text" : subType = "plain";
+                break;
+                case "image" : subType = "png";
+                break;
+                case "audio" : subType = "midi";
+                break;
+                case "video" : subType = "webm";
+                break;
+                case "application" : subType = "json";
+                break;
+            }
+        }
+
+        return type + "/" + subType;
+    }
+
     public static Map<String, String> parseRequestLine(String requestLine) {
         Map<String, String> tokenMap = new HashMap<>();
         String[] tokens = requestLine.split(" ");
+        String[] requestURI = tokens[1].split("\\?");
+
         tokenMap.put("method", tokens[0]);
-        tokenMap.put("path", tokens[1]);
         tokenMap.put("version", tokens[2]);
+        tokenMap.put("path", requestURI[0]);
+        tokenMap.put("queryString", "");
+
+        if (tokens[1].contains("?")) {
+            tokenMap.put("queryString", requestURI[1]);
+        }
+
         return tokenMap;
     }
 
