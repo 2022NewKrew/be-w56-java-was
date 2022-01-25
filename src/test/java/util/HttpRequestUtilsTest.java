2f4b226021a453a70996fd55ba9d72ddddc34e3e
HyunJi <hyunjilim98@gmail.com>
2022-01-25 15:18:17 +0900

    웹 서버 구현 2단계 테스트 수정

--- src/test/java/util/HttpRequestUtilsTest.java
+++ src/test/java/util/HttpRequestUtilsTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.*;
 import java.util.Map;
 
 
+import model.Request;
 import org.junit.jupiter.api.Test;
 import util.HttpRequestUtils.Pair;
 
@@ -73,6 +74,18 @@ public class HttpRequestUtilsTest {
     @Test
     public void parseUrl() {
         String url = "GET /index.html HTTP/1.1";
-        assertThat(HttpRequestUtils.parseRequestHeader(url)).isEqualTo("/index.html");
+        Request request = HttpRequestUtils.parseRequestHeader(url);
+
+        assertThat(request.getUrl()).isEqualTo("/index.html");
+        assertThat(request.getQuery()).isNull();
+    }
+
+    @Test
+    public void parseUrlWithQuery() {
+        String url = "GET /index.html?name=test HTTP/1.1";
+        Request request = HttpRequestUtils.parseRequestHeader(url);
+
+        assertThat(request.getUrl()).isEqualTo("/index.html");
+        assertThat(request.getQuery()).isEqualTo("name=test");
     }
 }
