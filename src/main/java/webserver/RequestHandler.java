5c93284e1110ff6c8586019ced55f11a9748556f
SaeGon-Heo <her84562@gmail.com>
2022-01-25 17:06:40 +0900

    ResponseWriter 별도로 추출

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -6,18 +6,17 @@ import model.ValueMap;
 import model.request.Headers;
 import model.request.HttpLocation;
 import model.request.HttpMethod;
-import org.apache.tika.Tika;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.HttpRequestUtils;
 import util.Separator;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -25,17 +24,10 @@ import java.util.Map;
 import java.util.concurrent.Callable;
 
 public class RequestHandler implements Callable<Void> {
-    private static final String FILE_PREFIX = "./webapp";
+    private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
     private static final String TOP_HEADER_SEPARATOR = " ";
     private static final String LOCATION_AND_PARAMETER_SEPARATOR = "\\?";
-    private static final String HEADER_SEPARATOR = ": ";
-
-    private static final String HEADER_NEWLINE = "\r\n";
-
-    private static final String RESPONSE_TOP_HEADER_OK = "HTTP/1.1 200 OK \r\n";
-
-    private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
     private final Socket connection;
     private final ResponseWriter responseWriter;
@@ -63,10 +55,10 @@ public class RequestHandler implements Callable<Void> {
                     location);
 
             if (method == HttpMethod.GET) {
-                writeFileResponse(out, location);
+                responseWriter.writeFileResponse(out, location);
             }
             else {
-                writeErrorResponse(out);
+                responseWriter.writeErrorResponse(out);
             }
         } catch (IOException e) {
             log.error(e.getMessage());
@@ -126,56 +118,5 @@ public class RequestHandler implements Callable<Void> {
             list.add(HttpRequestUtils.parseHeader(header));
         }
         return new Headers(list);
-    }
-
-    private void writeFileResponse(final OutputStream out, final String filePath) throws IOException {
-        final File file = new File(FILE_PREFIX + filePath);
-        if (file.exists() && file.isFile()) {
-            final Path path = file.toPath();
-            final Tika tika = new Tika();
-            final byte[] body = Files.readAllBytes(path);
-            writeResponse(out, body, tika.detect(path));
-
-            return;
-        }
-
-        writeErrorResponse(out);
-    }
-
-    private void writeErrorResponse(final OutputStream out) {
-        byte[] body = "Error".getBytes(StandardCharsets.UTF_8);
-        writeResponse(out, body, "text/plain");
-    }
-
-    private void writeResponse(final OutputStream out, final byte[] body, final String mime) {
-        final DataOutputStream dos = new DataOutputStream(out);
-
-        response200Header(dos, body.length, mime);
-        responseBody(dos, body);
-    }
-
-    private String getHeaderString(final String key, final String value) {
-        return key + HEADER_SEPARATOR + value + HEADER_NEWLINE;
-    }
-
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent, final String mime) {
-        try {
-            dos.writeBytes(RESPONSE_TOP_HEADER_OK);
-            dos.writeBytes(getHeaderString("Content-Type", mime + ";charset=utf-8"));
-            dos.writeBytes(getHeaderString("Content-Length", String.valueOf(lengthOfBodyContent)));
-            dos.writeBytes(HEADER_NEWLINE);
-            dos.flush();
-        } catch (IOException e) {
-            log.error("response200Header: " + e.getMessage());
-        }
-    }
-
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
-        } catch (IOException e) {
-            log.error("responseBody: " + e.getMessage());
-        }
     }
 }
