acb73ae14b06c30a27a0b7e246132774f3af4b53
jb.jang <jb.jang@kakaocorp.com>
2022-01-25 14:45:17 +0900

    feat: RequestHandler 에서 request 및 response 처리 분리

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,8 +2,6 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
-import java.util.List;
-import java.util.Map;
 
 import controller.Controller;
 import org.slf4j.Logger;
@@ -11,6 +9,7 @@ import org.slf4j.LoggerFactory;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
+    private static final Controller controller = new Controller();
 
     private Socket connection;
 
@@ -19,57 +18,12 @@ public class RequestHandler extends Thread {
     }
 
     public void run() {
-        log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
-                connection.getPort());
+        log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(), connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
-
-            // request line 처리
-            String line = br.readLine();
-            if (line == null) return;
-            log.debug("request line : {}", line);
-            String[] headerTokens = line.split(" ");
-            String httpMethod = headerTokens[0];
-            String url = headerTokens[1];
-            String protocol = headerTokens[2];
-
-            // header 처리
-            line = br.readLine();
-            while (!line.equals("")) {
-                log.debug("header : {}", line);
-                line = br.readLine();
-            }
-
-
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            ParsedRequest parsedRequest = new ParsedRequest(in);
             DataOutputStream dos = new DataOutputStream(out);
-            Path filePath = Paths.get("./webapp" + url);
-            File file = new File(filePath.toString());
-            String mimeType = new Tika().detect(file);
-            byte[] body = Files.readAllBytes(filePath);
-            response200Header(dos, mimeType, body.length);
-            responseBody(dos, body);
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
-
-    private void response200Header(DataOutputStream dos, String mimeType, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: " + mimeType + ";charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
-
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
+            controller.makeResponse(parsedRequest, dos);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
