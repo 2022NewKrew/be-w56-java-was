6773e0c5386243239e93baa7f08c4da413d74ffe
chen.kim <fromecha@gmail.com>
2022-01-25 13:00:38 +0900

    [feat] httprequest 클래스들 생성

--- src/main/java/request/HttpRequest.java
+++ src/main/java/request/HttpRequest.java
@@ -1,2 +1,11 @@
-package request;public class HttpRequest {
+package request;
+
+public class HttpRequest {
+    private final HttpRequestLine httpRequestLine;
+    private final HttpRequestHeader httpRequestHeader;
+
+    public HttpRequest(HttpRequestLine httpRequestLine, HttpRequestHeader httpRequestHeader) {
+        this.httpRequestLine = httpRequestLine;
+        this.httpRequestHeader = httpRequestHeader;
+    }
 }
--- src/main/java/request/HttpRequestHeader.java
+++ src/main/java/request/HttpRequestHeader.java
@@ -1,2 +1,15 @@
-package request;public class HttpRequestHeader {
+package request;
+
+import java.util.Map;
+
+public class HttpRequestHeader {
+    private final Map<String, String> query;
+
+    public HttpRequestHeader(Map<String, String> query) {
+        this.query = query;
+    }
+
+    public Map<String, String> getQuery() {
+        return query;
+    }
 }
