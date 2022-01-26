5aca11f85b63d2f0591e4ece545eb16c3026003d
geatrigger-kim <geatrigger.kim@kakaocorp.com>
2022-01-26 10:45:04 +0900

    Store all header informaion in requestMap

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,23 +3,22 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.HttpRequestUtils;
 
-import static util.HttpRequestUtils.getUrlExtension;
-import static util.HttpRequestUtils.parseRequestLine;
+import static util.HttpRequestUtils.*;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
+    private Map<String, String> requestMap;
     private Socket connection;
 
     public RequestHandler(Socket connectionSocket) {
+        requestMap = new HashMap<>();
         this.connection = connectionSocket;
     }
 
@@ -31,32 +30,37 @@ public class RequestHandler extends Thread {
             BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
             String line = br.readLine();
             log.debug(line);
-            HttpRequestUtils.Pair urlPair = parseRequestLine(line);
-            String url = urlPair.getValue();
-            String extension = getUrlExtension(url);
+            parseRequestLine(line).forEach(pair -> requestMap.put(pair.getKey(), pair.getValue()));
             while(!line.equals("")) {
                 if (line == null) {
                     break;
                 }
                 line = br.readLine();
-                log.debug(line);
+//                log.debug(line);
+                Pair headerPair = parseHeader(line);
+                if (headerPair == null) {
+                    continue;
+                }
+                requestMap.put(headerPair.getKey(), headerPair.getValue());
             }
+            log.debug(requestMap.toString());
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+            byte[] body = Files.readAllBytes(new File("./webapp" + requestMap.get("Url")).toPath());
             log.debug("Response!");
-            response200Header(dos, body.length, extension);
+            response200Header(dos, body.length, requestMap);
             responseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent, String extension) {
+    private void response200Header(DataOutputStream dos, int lengthOfBodyContent, Map<String, String> requestMap) {
         try {
             String contentType = "";
             List<String> textTypeList = new ArrayList<>(Arrays.asList("css", "html", "js"));
             List<String> imageTypeList = new ArrayList<>(Arrays.asList("ico", "png", "jpeg", "webp"));
+            String extension = getUrlExtension(requestMap.get("Url"));
             contentType = textTypeList.stream().anyMatch(s -> s.equals(extension)) ? "text/" + extension : contentType;
             contentType = imageTypeList.stream().anyMatch(s -> s.equals(extension)) ? "image/" + extension : contentType;
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
