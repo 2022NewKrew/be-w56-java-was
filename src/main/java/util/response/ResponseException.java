a37e8ba074eb052c8753f1424138c49c2b04e914
david.dh <david.dh@kakaocorp.com>
2022-01-26 19:08:13 +0900

    예외 처리 담당 Response 클래스 구현

---
+++ src/main/java/util/response/ResponseException.java
@@ -0,0 +1,29 @@
+package util.response;
+
+import util.HttpStatus;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class ResponseException {
+    public static Response notFoundResponse() throws IOException {
+        byte[] body = Files.readAllBytes(new File("./webapp/404.html").toPath());
+        return new ResponseBuilder()
+                .setHttpStatus(HttpStatus.NOT_FOUND)
+                .addHeader("Content-Type","text/html;charset=utf-8")
+                .addHeader("Content-length", String.valueOf(body.length))
+                .setBody(body)
+                .build();
+    }
+
+    public static Response inputErrorResponse() throws IOException {
+        byte[] body = Files.readAllBytes(new File("./webapp/error.html").toPath());
+        return new ResponseBuilder()
+                .setHttpStatus(HttpStatus.BAD_REQUEST)
+                .addHeader("Content-Type","text/html;charset=utf-8")
+                .addHeader("Content-length", String.valueOf(body.length))
+                .setBody(body)
+                .build();
+    }
+}
