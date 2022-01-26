ca210f1ae543eefe09609c293a89e23dd23a1f68
MELT <melt.ep00@gmail.com>
2022-01-26 12:04:40 +0900

    간단 웹 서버 3단계 구현 - `RequestHeader` 추가

---
+++ src/main/java/controller/request/RequestHeader.java
@@ -0,0 +1,34 @@
+package controller.request;
+
+import util.HttpRequestUtils;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by melodist
+ * Date: 2022-01-26 026
+ * Time: 오전 11:19
+ */
+public class RequestHeader {
+
+    private Map<String, String> paramMap;
+
+    private RequestHeader(Map<String, String> paramMap) {
+        this.paramMap = paramMap;
+    }
+
+    public static RequestHeader of(List<String> requestHeaderStrings) {
+        Map<String, String> paramMap = new HashMap<>();
+        for (String requestHeaderString : requestHeaderStrings) {
+            HttpRequestUtils.Pair pair = HttpRequestUtils.parseHeader(requestHeaderString);
+            paramMap.put(pair.getKey(), pair.getValue());
+        }
+        return new RequestHeader(paramMap);
+    }
+
+    public String getParameter(String key) {
+        return paramMap.get(key);
+    }
+}
