8440436eb3859c1953820ed06ea77ae3c4aae5b4
JuyeoungJun <wjfhs01@gmail.com>
2022-01-26 11:17:41 +0900

    feat: requestLine이 header 보다 먼저 오도록 변경 (step2 리뷰 반영)

--- src/main/java/http/HttpRequest.java
+++ src/main/java/http/HttpRequest.java
@@ -4,8 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 
 public class HttpRequest {
-    private HttpHeader header;
     private RequestLine requestLine;
+    private HttpHeader header;
     private RequestBody requestBody;
 
     public HttpRequest(BufferedReader bufferedReader) throws IOException {
