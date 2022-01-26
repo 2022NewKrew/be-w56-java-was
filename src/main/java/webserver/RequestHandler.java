70397ef74a00579e617464ccf3b38489220eabf5
jb.jang <jb.jang@kakaocorp.com>
2022-01-26 16:48:04 +0900

    feat: RequestHandler 요청 처리 로직 변경

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -4,14 +4,16 @@ import java.io.*;
 import java.net.Socket;
 
 import controller.Controller;
+import http.HttpRequest;
+import http.HttpResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
-    private static final Controller controller = new Controller();
 
     private Socket connection;
+    private RequestMapper requestMapper = new RequestMapper();
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
@@ -21,9 +23,18 @@ public class RequestHandler extends Thread {
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(), connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            ParsedRequest parsedRequest = new ParsedRequest(in);
-            DataOutputStream dos = new DataOutputStream(out);
-            controller.makeResponse(parsedRequest, dos);
+            // http 요청과 응답을 만들어준다.
+            HttpRequest request = new HttpRequest(in);
+            HttpResponse response = new HttpResponse(out);
+
+            // url 에 맞는 컨트롤러를 찾아준다.
+            String url = request.getUrl();
+            if (url == null) return;
+            Controller controller = requestMapper.mapping(url);
+
+            // 컨트롤러가 응답을 처리한다.
+            controller.makeResponse(request, response);
+
         } catch (IOException e) {
             log.error(e.getMessage());
         }
