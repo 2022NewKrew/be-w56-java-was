657567a5c2a61f369ef6c64fc9a0a583a030e25f
dfjung4254 <rmsghk4254@naver.com>
2022-01-26 19:09:34 +0900

    기본 웹서버 initial

--- src/main/java/util/IOUtils.java
+++ src/main/java/util/IOUtils.java
@@ -5,11 +5,10 @@ import java.io.IOException;
 import java.util.Optional;
 
 public class IOUtils {
+
   /**
-   * @param BufferedReader는
-   *            Request Body를 시작하는 시점이어야
-   * @param contentLength는
-   *            Request Header의 Content-Length 값이다.
+   * @param BufferedReader는 Request Body를 시작하는 시점이어야
+   * @param contentLength는  Request Header의 Content-Length 값이다.
    * @return
    * @throws IOException
    */
@@ -18,4 +17,18 @@ public class IOUtils {
     br.read(body, 0, contentLength);
     return String.copyValueOf(body);
   }
+
+
+  /**
+   * 파일 확장자 가져 옴.
+   *
+   * @param fileName
+   * @return
+   */
+  public static Optional<String> getExtension(String fileName) {
+    return Optional.ofNullable(fileName)
+        .filter(f -> f.contains("."))
+        .map(f -> f.substring(fileName.lastIndexOf(".") + 1));
+  }
+
 }
