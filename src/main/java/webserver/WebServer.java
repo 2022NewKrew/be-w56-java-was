d911257e4a75a5035ecddf8c532add0dd11fdbaa
Jungyu Choi <devwithpug@kakao.com>
2022-01-26 18:01:35 +0900

    feat: ThreadPoolExecutor 를 통해 스레드풀 생성

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -2,6 +2,8 @@ package webserver;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +12,7 @@ import webserver.handler.RequestHandler;
 public class WebServer {
     private static final Logger log = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 8080;
+    private static final int DEFAULT_THREAD_POOL_SIZE = 10;
     public static final String DEFAULT_RESOURCES_DIR = "./webapp";
 
     public static void main(String args[]) throws Exception {
@@ -20,6 +23,9 @@ public class WebServer {
             port = Integer.parseInt(args[0]);
         }
 
+        // 스레드풀 생성
+        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(DEFAULT_THREAD_POOL_SIZE);
+
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
 
         try (ServerSocket listenSocket = new ServerSocket(port)) {
@@ -29,7 +35,7 @@ public class WebServer {
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
                 RequestHandler requestHandler = new RequestHandler(connection);
-                requestHandler.start();
+                executor.submit(requestHandler);
             }
         }
     }
