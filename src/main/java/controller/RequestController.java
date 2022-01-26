20aa37a74df34e10ef3556b483aa1640f99adc2a
keith.lee <keith.lee@kakaocorp.com>
2022-01-26 21:12:30 +0900

    4단계 구현 완료

--- src/main/java/controller/RequestController.java
+++ src/main/java/controller/RequestController.java
@@ -13,7 +13,6 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Map;
 
 public class RequestController {
@@ -23,16 +22,32 @@ public class RequestController {
     public static HttpResponse controlRequest(HttpRequest httpRequest) {
         String path = httpRequest.getPath();
 
-        if (path.startsWith("/user/create")) {
+        if (path.equals("/user/create")) {
             Map<String, String> params = httpRequest.getParameters();
             try {
                 User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
                 log.debug("User: {}", user);
                 DataBase.addUser(user);
                 return generateResponse302("/user/list.html");
+
             } catch (Exception e) {
                 return generateResponse400(e);
             }
+        } else if (path.equals("/user/login")) {
+            Map<String, String> params = httpRequest.getParameters();
+            String userId = params.get("userId");
+            String password = params.get("password");
+            User user = DataBase.findUserById(userId);
+            if (user == null) {
+                log.debug("Login failed: User not found");
+                return generateLoginFailedResponse();
+            }
+            if (user.getPassword().equals(password)) {
+                log.debug("Login success!");
+                return generateLoginResponse();
+            }
+            log.debug("Login failed: Password mismatch");
+            return generateLoginFailedResponse();
         } else if (Arrays.stream(MIME.values()).anyMatch(mime -> mime.isExtensionMatch(path))) {
             return generateStaticResponse(path);
         } else {
@@ -53,6 +68,22 @@ public class RequestController {
             log.error(e.getMessage());
             return generateResponse500();
         }
+    }
+
+    private static HttpResponse generateLoginResponse() {
+        return HttpResponse.builder()
+                .status(HttpStatus.FOUND)
+                .header("Location", "/index.html")
+                .header("Set-Cookie", "logined=true; Path=/")
+                .build();
+    }
+
+    private static HttpResponse generateLoginFailedResponse() {
+        return HttpResponse.builder()
+                .status(HttpStatus.FOUND)
+                .header("Location", "/user/login_failed.html")
+                .header("Set-Cookie", "logined=false; Path=/")
+                .build();
     }
 
     private static HttpResponse generateResponse302(String path) {
