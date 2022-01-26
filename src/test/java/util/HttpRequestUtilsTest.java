45ada464fd9b1d502cd38b1b7765eb3f0e3ff722
anika.dovob <anika.dovob@kakaocorp.com>
2022-01-26 09:49:56 +0900

    test: Utils test 추가

--- src/test/java/util/HttpRequestUtilsTest.java
+++ src/test/java/util/HttpRequestUtilsTest.java
@@ -33,6 +33,15 @@ public class HttpRequestUtilsTest {
     }
 
     @Test
+    void parseHttpMethod_invalid() {
+        String methodToken = "GETT";
+
+        assertThatThrownBy(() -> HttpRequestUtils.parseHttpMethod(methodToken))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("No enum constant http.HttpMethod." + methodToken);
+    }
+
+    @Test
     void parsePath() {
         String targetToken = "/users/create?userId=javajigi";
 
@@ -84,19 +93,19 @@ public class HttpRequestUtilsTest {
     }
 
     @Test
-    public void getKeyValue() throws Exception {
+    public void getKeyValue() {
         Pair pair = HttpRequestUtils.getKeyValue("userId=javajigi", "=");
         assertThat(pair).isEqualTo(new Pair("userId", "javajigi"));
     }
 
     @Test
-    public void getKeyValue_invalid() throws Exception {
+    public void getKeyValue_invalid() {
         Pair pair = HttpRequestUtils.getKeyValue("userId", "=");
         assertThat(pair).isNull();
     }
 
     @Test
-    public void parseHeader() throws Exception {
+    public void parseHeader() {
         String header = "Content-Length: 59";
         Pair pair = HttpRequestUtils.parseHeader(header);
         assertThat(pair).isEqualTo(new Pair("Content-Length", "59"));
--- src/test/java/util/IOUtilsTest.java
+++ src/test/java/util/IOUtilsTest.java
@@ -1,14 +1,29 @@
 package util;
 
+import static org.assertj.core.api.Assertions.*;
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.StringReader;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class IOUtilsTest {
+
     private static final Logger logger = LoggerFactory.getLogger(IOUtilsTest.class);
+
+    @Test
+    public void getRequestLines() throws IOException {
+        String httpRequestHeader = "GET /index.html HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\n";
+        StringReader sr = new StringReader(httpRequestHeader);
+
+        List<String> requestLines = IOUtils.getRequestLines(new BufferedReader(sr));
+
+        assertThat(requestLines).hasSize(3);
+    }
 
     @Test
     public void readData() throws Exception {
