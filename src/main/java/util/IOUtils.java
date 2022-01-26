3d8ac64545c0df7c3ea3cc498f1509f4e325fe63
david.dh <david.dh@kakaocorp.com>
2022-01-26 19:05:19 +0900

    request body 로그 출력

--- src/main/java/util/IOUtils.java
+++ src/main/java/util/IOUtils.java
@@ -1,9 +1,16 @@
 package util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import webserver.RequestHandler;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 
 public class IOUtils {
+
+    private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
+
     /**
      * @param br
      *            Request Body 를 시작하는 시점이어야
@@ -17,6 +24,8 @@ public class IOUtils {
         if (result == -1) {
             return null;
         }
-        return String.copyValueOf(body);
+        String bodyString = String.copyValueOf(body);
+        log.debug("request body : {}",bodyString);
+        return bodyString;
     }
 }
