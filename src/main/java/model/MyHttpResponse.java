2bbc34677dcea8f5e5f7a7377969a5f2799cb281
ben.ny <ben.ny@kakaocorp.com>
2022-01-26 23:36:27 +0900

    Feature : GET으로 회원가입 기능 구현
    - UserController 생성
    - processStaticUri 메소드로 잘못된 static 파일 루트 수정

--- src/main/java/model/MyHttpResponse.java
+++ src/main/java/model/MyHttpResponse.java
@@ -1,5 +1,6 @@
 package model;
 
+import Controller.UserController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import webserver.enums.HttpStatus;
@@ -11,24 +12,56 @@ import java.nio.file.Files;
 public class MyHttpResponse {
     private static final Logger logger = LoggerFactory.getLogger(MyHttpResponse.class);
     private static final String DEFAULT_PATH = "./webapp";
+    private static final String MAIN_PAGE = "./webapp/index.html";
     private final String statusLine;
     private byte[] body;
+
+    private final UserController userController = new UserController();
 
     public MyHttpResponse(MyHttpRequest myHttpRequest) throws IOException {
         HttpStatus httpStatus = HttpStatus.OK;
 
         if (myHttpRequest.getMethod().equals("GET")) {
-            try {
-                body = Files.readAllBytes(new File(DEFAULT_PATH + myHttpRequest.getUri()).toPath());
-            } catch (IOException e) {
-                httpStatus = HttpStatus.NOT_FOUND;
-            }
+            httpStatus = get(myHttpRequest);
         }
         else if (myHttpRequest.getMethod().equals("POST")) {
             httpStatus = HttpStatus.CREATED;
         }
 
         statusLine = httpStatus.makeStatusLine(myHttpRequest.getProtocol());
+    }
+
+    private HttpStatus get(MyHttpRequest request) {
+        try {
+            String uri = request.getUri();
+            uri = processStaticUri(uri);
+
+            if (uri.equals("/")) {
+                body = Files.readAllBytes(new File(MAIN_PAGE).toPath());
+            }
+            else if (uri.equals("/user/create")) {
+                userController.signUp(request.getParameters());
+                body = Files.readAllBytes(new File(MAIN_PAGE).toPath());
+            }
+            else {
+                body = Files.readAllBytes(new File(DEFAULT_PATH + uri).toPath());
+            }
+            return HttpStatus.OK;
+        } catch (IOException e) {
+            return HttpStatus.NOT_FOUND;
+        }
+    }
+
+    private String processStaticUri(String uri) {
+        String[] staticUri= {"/css", "/js", "/fonts", "/images"};
+        for (String s : staticUri) {
+            if (uri.contains(s)) {
+                uri = uri.substring(1);
+                int idx = uri.indexOf('/');
+                return uri.substring(idx);
+            }
+        }
+        return uri;
     }
 
     public String getStatusLine() {
