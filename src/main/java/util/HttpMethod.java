e7bd88480a2953ec021f77e324d7a1737396b877
frank.burger <frank.burger@kakaocorp.com>
2022-01-26 20:23:30 +0900

    refactor: Optional.ofNullable 적용

--- src/main/java/util/HttpMethod.java
+++ src/main/java/util/HttpMethod.java
@@ -6,25 +6,22 @@ import lombok.ToString;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @ToString
 public enum HttpMethod {
-    GET,
-    POST,
-    PUT,
-    DELETE;
+    GET, POST, PUT, DELETE;
 
     private static final Map<String, HttpMethod> STRING_HTTP_METHOD_MAP;
+
     static {
         STRING_HTTP_METHOD_MAP = new HashMap<>();
-        Arrays.stream(HttpMethod.values()).forEach(value -> STRING_HTTP_METHOD_MAP.put(value.name().toUpperCase(), value));
+        Arrays.stream(HttpMethod.values())
+              .forEach(value -> STRING_HTTP_METHOD_MAP.put(value.name().toUpperCase(), value));
     }
 
     public static HttpMethod getHttpMethod(String httpMethodAsString) {
-        HttpMethod httpMethod = STRING_HTTP_METHOD_MAP.get(httpMethodAsString.toUpperCase());
-        if (httpMethod == null) {
-            throw new UnsupportedHttpMethodException();
-        }
-        return httpMethod;
+        return Optional.ofNullable(STRING_HTTP_METHOD_MAP.get(httpMethodAsString.toUpperCase()))
+                       .orElseThrow(UnsupportedHttpMethodException::new);
     }
 }
