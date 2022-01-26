c9fda441c12e7265fdc2db6e01f8581ec5494060
charlie.p1 <charlie.p1@kakaocorp.com>
2022-01-26 16:34:05 +0900

    Add body property at HttpRequest

--- src/main/java/com/kakao/http/request/HttpRequest.java
+++ src/main/java/com/kakao/http/request/HttpRequest.java
@@ -1,22 +1,27 @@
 package com.kakao.http.request;
 
+import com.kakao.http.header.BasicHeader;
+import com.kakao.http.header.HttpHeader;
 import com.kakao.util.HttpRequestUtils;
+import com.kakao.util.IOUtils;
 import lombok.Getter;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
+import java.util.Optional;
 
 @Getter
 public class HttpRequest {
     private final HttpMethod method;
     private final Url url;
     private final String version;
-    private final Map<String, String> headerMap;
+    private final List<HttpHeader> headers;
+    private final String body;
 
     public HttpRequest(InputStream in) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(in));
@@ -24,17 +29,27 @@ public class HttpRequest {
         this.method = HttpMethod.valueOf(requestLine.getMethod());
         this.url = new Url(requestLine.getUrl());
         this.version = requestLine.getVersion();
-        this.headerMap = parseHeaderMap(br);
+        this.headers = parseHeaderList(br);
+        this.body = parseRequestBody(br);
     }
 
-    private Map<String, String> parseHeaderMap(BufferedReader bufferedReader) throws IOException {
-        Map<String, String> tempMap = new HashMap<>();
+    private String parseRequestBody(BufferedReader br) throws IOException {
+        Optional<HttpHeader> contentLengthHeader = this.headers.stream()
+                .filter(header -> header.key().equalsIgnoreCase("Content-Length"))
+                .findFirst();
+        return contentLengthHeader.isEmpty()
+                ? null
+                : IOUtils.readData(br, Integer.parseInt(contentLengthHeader.get().value()));
+    }
+
+    private List<HttpHeader> parseHeaderList(BufferedReader bufferedReader) throws IOException {
+        List<HttpHeader> headers = new ArrayList<>();
         String headerLine = bufferedReader.readLine();
         while (headerLine != null && !"".equals(headerLine)) {
             HttpRequestUtils.Pair pair = HttpRequestUtils.parseHeader(headerLine);
-            tempMap.put(pair.getKey(), pair.getValue());
+            headers.add(new BasicHeader(pair.getKey(), pair.getValue()));
             headerLine = bufferedReader.readLine();
         }
-        return Collections.unmodifiableMap(tempMap);
+        return Collections.unmodifiableList(headers);
     }
 }
