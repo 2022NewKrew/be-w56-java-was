c484d52ff43e23e924b1ff0441eb220bcc6e55bc
yuchanleeme <yuchanlee.me@gmail.com>
2022-01-27 16:09:46 +0900

    feat: 로그인하면 쿠키에 세팅되는 코드 추가
    - 로그인하면 쿠키에 logined=true 설정

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -5,7 +5,15 @@ import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.HttpRequestUtils;
+import util.Pair;
+import web.http.request.HttpRequest;
+import web.http.request.HttpRequestHeaders;
+import web.http.request.HttpRequestLine;
+import web.http.response.*;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Map;
 
 public class UserService {
@@ -22,5 +30,34 @@ public class UserService {
         User user = new User(userId, password, name, email);
         log.info("Insert User : {} ", user);
         DataBase.addUser(user);
+    }
+
+    public static HttpResponse signUp(HttpRequest httpRequest) {
+        HttpRequestLine requestLine = httpRequest.getHttpRequestLine();
+        String requestBody = httpRequest.getBodyData();
+
+        addUser(requestBody);
+
+        HttpResponseStatusLine statusLine = new HttpResponseStatusLine(requestLine.getVersion(), HttpStatus.REDIRECT);
+        HttpResponseHeaders headers = new HttpResponseHeaders();
+        headers.addHeader(new Pair("Location", "http://localhost:8080/index.html"));
+
+        HttpResponseBody body = new HttpResponseBody();
+
+        return new HttpResponse(statusLine, headers, body);
+    }
+
+    public static HttpResponse login(HttpRequest httpRequest) {
+        HttpRequestLine requestLine = httpRequest.getHttpRequestLine();
+
+        HttpResponseStatusLine statusLine = new HttpResponseStatusLine(requestLine.getVersion(), HttpStatus.REDIRECT);
+        HttpResponseHeaders headers = new HttpResponseHeaders();
+
+        headers.addHeader(new Pair("Set-Cookie", "logined=true; Path=/"));
+        headers.addHeader(new Pair("Location", "http://localhost:8080/index.html"));
+
+        HttpResponseBody body = new HttpResponseBody();
+
+        return new HttpResponse(statusLine, headers, body);
     }
 }
