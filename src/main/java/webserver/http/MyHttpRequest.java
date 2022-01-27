4a647187cafb989dfd64c3674de89976f057e8ff
Jungyu Choi <devwithpug@kakao.com>
2022-01-27 11:35:43 +0900

    step-3: MyHttpRequest request body 값 추가

--- src/main/java/webserver/http/MyHttpRequest.java
+++ src/main/java/webserver/http/MyHttpRequest.java
@@ -1,5 +1,8 @@
 package webserver.http;
 
+import util.IOUtils;
+import webserver.exception.BadRequestException;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.net.URI;
@@ -19,18 +22,27 @@ public class MyHttpRequest extends HttpRequest {
     private static final String HEADER_KEY_VALUE_DELIMITER = ": ";
     private static final String HEADER_VALUE_DELIMITER = ",";
 
-    private final String method;
+    private final HttpMethod method;
     private final String requestURI;
     private final String version;
     private final Map<String, List<String>> headers;
+    private final String body;
 
     private MyHttpRequest(BufferedReader in) throws IOException {
         String[] requestHeaderParams = in.readLine().split(REQUEST_LINE_DELIMITER);
-        this.method = requestHeaderParams[0];
+        validateRequestHeader(requestHeaderParams);
+        this.method = HttpMethod.valueOf(requestHeaderParams[0]);
         this.requestURI = requestHeaderParams[1];
         this.version = requestHeaderParams[2];
         this.headers = new HashMap<>();
         initHeaders(in);
+        this.body = IOUtils.readData(in, contentLength());
+    }
+
+    private void validateRequestHeader(String[] requestHeaderParams) {
+        if (requestHeaderParams.length != 3) {
+            throw new BadRequestException("에러: 요청 헤더가 적절하지 않습니다.");
+        }
     }
 
     private void initHeaders(BufferedReader in) throws IOException {
@@ -46,19 +58,27 @@ public class MyHttpRequest extends HttpRequest {
         }
     }
 
+    private int contentLength() {
+        if (headers.containsKey("Content-Length")) {
+            List<String> values = headers.get("Content-Length");
+            return Integer.parseInt(values.get(0));
+        }
+        return 0;
+    }
+
     public static MyHttpRequest of(BufferedReader in) throws IOException {
         return new MyHttpRequest(in);
     }
 
     @Override
     public Optional<BodyPublisher> bodyPublisher() {
-        // TODO - Request Body 구현
-        return Optional.empty();
+        // 구현 X
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public String method() {
-        return method;
+        return method.name();
     }
 
     @Override
@@ -92,6 +112,10 @@ public class MyHttpRequest extends HttpRequest {
     @Override
     public HttpHeaders headers() {
         return HttpHeaders.of(headers, ALLOWED_ALL_HEADERS);
+    }
+
+    public String body() {
+        return body;
     }
 
     @Override
