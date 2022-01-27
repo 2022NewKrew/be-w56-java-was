1a064501e5250cc309b85ee702f97d8911d22b3c
kimdaeseon <kimdaeseon@khu.ac.kr>
2022-01-27 17:09:22 +0900

    RequestStartLine 에따른 HttpRequest 변경
    -- RequestStartLine 필드 추가
    -- get 메서드 (Url, Query, Method)의 내부 구현 변경 (header -> startLine)

--- src/main/java/http/request/HttpRequest.java
+++ src/main/java/http/request/HttpRequest.java
@@ -6,28 +6,30 @@ import webserver.HttpMethod;
 
 public class HttpRequest {
 
+    private final RequestStartLine startLine;
     private final RequestHeader header;
     private final RequestBody body;
 
-    public HttpRequest(String header, String body) {
+    public HttpRequest(RequestStartLine startLine, RequestHeader header, RequestBody body) {
         try {
-            this.header = new RequestHeader(header);
-            this.body = new RequestBody(body);
+            this.startLine = startLine;
+            this.header = header;
+            this.body = body;
         } catch (Exception exception) {
             throw new BadRequestException(exception.getMessage());
         }
     }
 
     public String getUrl() {
-        return header.getUrl();
+        return startLine.getUrl();
     }
 
     public Map<String, String> getQuery() {
-        return header.getQuery();
+        return startLine.getQuery();
     }
 
     public HttpMethod getMethod() {
-        return header.getMethod();
+        return startLine.getMethod();
     }
 
     public Map<String, String> getBodyData() {
