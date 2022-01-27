988d43179df2a4e5309003495c47e33164719e42
kimdaeseon <kimdaeseon@khu.ac.kr>
2022-01-27 18:53:15 +0900

    ViewMaker 코드 수정
    -- 파일을 찾지 못하면 NotFoundException을 던짐

--- src/main/java/view/ViewMaker.java
+++ src/main/java/view/ViewMaker.java
@@ -1,32 +1,39 @@
 package view;
 
 import com.google.common.io.Files;
+import exception.NotFoundException;
 import java.io.File;
-import java.io.IOException;
 import java.util.Map;
 
 public class ViewMaker {
 
-    public static byte[] getView(String filePath, Map<String, String> model) throws IOException {
-        byte[] body = Files.toByteArray(new File("./webapp" + filePath));
-
+    public static byte[] getView(String filePath, Map<String, String> model) {
+        byte[] body = getFile("./webapp" + filePath);
         if (model.isEmpty()) {
             return body;
         }
         return applyModel(body, model);
     }
 
-    public static byte[] getNotFoundView() throws IOException {
-        return Files.toByteArray(new File("./webapp/notFound.html"));
+    public static byte[] getNotFoundView() {
+        return getFile("./webapp/notFound.html");
     }
 
-    public static byte[] getBadRequestView() throws IOException {
-        return Files.toByteArray(new File("./webapp/badRequest.html"));
+    public static byte[] getBadRequestView() {
+        return getFile("./webapp/badRequest.html");
     }
 
     private static byte[] applyModel(byte[] body, Map<String, String> model) {
         //미구현
         String stringBody = new String(body);
         return stringBody.getBytes();
+    }
+
+    private static byte[] getFile(String filePath) {
+        try {
+            return Files.toByteArray(new File(filePath));
+        } catch (Exception exception) {
+            throw new NotFoundException(exception.getMessage());
+        }
     }
 }
