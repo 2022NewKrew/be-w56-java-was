3f2c2ef38e7e318030c1e4f19b119248e99de984
mylo.l <mylo.l@kakaocorp.com>
2022-01-27 19:30:33 +0900

    Step4. @RequestMapping 사용하여 Controller 리팩토링

---
+++ src/main/java/controller/UserController.java
@@ -0,0 +1,55 @@
+package controller;
+
+import http.request.Request;
+import model.ModelAndView;
+import model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import service.UserService;
+import util.RequestMapping;
+
+public class UserController implements Controller {
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
+    private final UserService userService = UserService.getInstance();
+
+    private static final UserController userController = new UserController();
+
+    private UserController(){}
+
+    public static UserController getInstance(){
+        return userController;
+    }
+
+
+    @RequestMapping(method = "GET", url = "/users/form")
+    public ModelAndView createUserForm(Request request) {
+        log.info("[UserController] : createUser");
+        return new ModelAndView("/users/form");
+    }
+
+    @RequestMapping(method = "POST", url = "/users")
+    public ModelAndView signupUser(Request request){
+        log.info("[UserController] : signupUser");
+        User user = userService.createUser(request.getRequestBody());
+        return new ModelAndView("redirect:/", "user", user);
+    }
+
+    @RequestMapping(method = "GET", url = "/users/login")
+    public ModelAndView createLoginForm(Request request){
+        log.info("[UserController] : createLoginForm");
+        return new ModelAndView("/users/login");
+    }
+
+    @RequestMapping(method = "POST", url = "/users/login")
+    public ModelAndView loginUser(Request request){
+        log.info("[UserController] : loginUser");
+        boolean login = userService.login(request.getRequestBody());
+        ModelAndView mv = new ModelAndView("redirect:/");
+        mv.addObject("login", login);
+        if(login){
+            mv.addObject("user", userService.searchUserById(request.getRequestBody()));
+        }
+        return mv;
+    }
+}
--- src/main/java/http/HttpStatusCode.java
+++ src/main/java/http/HttpStatusCode.java
@@ -35,30 +35,35 @@ public enum HttpStatusCode {
             if (mv.getValue("login") != null && mv.getValue("login").equals(true)){
                 dos.writeBytes("Set-Cookie: logined=" + mv.getValue("login") + "; Path=/\r\n");
             }
+            dos.flush();
         }
     },
     BAD_REQUEST(400, "Bad Request"){
         @Override
         public void response(DataOutputStream dos, ModelAndView mv) throws IOException {
             dos.writeBytes("HTTP/1.1 "+ getStatusCode() + " " + getMessage() + "\r\n");
+            dos.flush();
         }
     },
     UNAUTHROIZED(401, "Unauthorized"){
         @Override
         public void response(DataOutputStream dos, ModelAndView mv) throws IOException {
             dos.writeBytes("HTTP/1.1 "+ getStatusCode() + " " + getMessage() + "\r\n");
+            dos.flush();
         }
     },
     FORBIDDEN(403, "Forbidden"){
         @Override
         public void response(DataOutputStream dos, ModelAndView mv) throws IOException {
             dos.writeBytes("HTTP/1.1 "+ getStatusCode() + " " + getMessage() + "\r\n");
+            dos.flush();
         }
     },
     NOT_FOUND(404, "Not Found"){
         @Override
         public void response(DataOutputStream dos, ModelAndView mv) throws IOException {
             dos.writeBytes("HTTP/1.1 "+ getStatusCode() + " " + getMessage() + "\r\n");
+            dos.flush();
         }
     };
 
