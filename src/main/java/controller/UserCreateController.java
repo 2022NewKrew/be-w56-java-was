9107eed0771abdffce47d10f0c0a652a8fbbd4a6
MELT <melt.ep00@gmail.com>
2022-01-27 21:02:37 +0900

    간단 웹 서버 4단계 구현 - 요청 형식 변경에 따른 코드 수정

--- src/main/java/controller/UserCreateController.java
+++ src/main/java/controller/UserCreateController.java
@@ -2,8 +2,9 @@ package controller;
 
 import controller.request.Request;
 import controller.response.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import service.UserService;
-
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,6 +15,7 @@ import java.util.Map;
  * Time: 오후 4:22
  */
 public class UserCreateController implements WebController{
+    private static final Logger log = LoggerFactory.getLogger(UserCreateController.class);
 
     @Override
     public Response process(Request request) {
