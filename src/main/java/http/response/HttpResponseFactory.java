b8aee2e2ebc57d2ed9aae5ff4fdf295e2932e095
kimdaeseon <kimdaeseon@khu.ac.kr>
2022-01-27 18:54:44 +0900

    HttpResponseFactory 코드 수정
    기존에 상황에 맞는 객체를 만드는 로직을 HttpResponse 객체에 상황에 맞게 값을 추가하도록 변경

--- src/main/java/http/response/HttpResponseFactory.java
+++ src/main/java/http/response/HttpResponseFactory.java
@@ -1,42 +1,77 @@
 package http.response;
 
 import java.io.DataOutputStream;
-import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import view.ViewMaker;
 
 public class HttpResponseFactory {
 
-    public static HttpResponse getHttpResponse(Map<String, String> result,
-            Map<String, String> model, DataOutputStream dos) throws IOException {
-        try {
-            StatusCode statusCode = StatusCode.getStatusString(result.get("status"));
-            return getResponse(statusCode, result, model, dos);
-        } catch (IOException exception) {
-            return new HttpResponse404(dos, ViewMaker.getNotFoundView());
+    private static NewHttpResponse getHttpResponse(StatusCode statusCode,
+            Map<String, String> result,
+            Map<String, String> model, String protocol, DataOutputStream dos) {
+
+        String url = result.get("url");
+        String status = StatusCode
+                .getStatusString(result.get("status"))
+                .getStatus();
+        
+        ResponseStatusLine statusLine = new ResponseStatusLine(protocol, status);
+        ResponseHeader header = new ResponseHeader();
+
+        switch (statusCode) {
+            case OK:
+                return getHttpResponse200(statusLine, header, url, dos);
+            case FOUND:
+                return getHttpResponse302(statusLine, header, url, dos);
+            case UNAUTHORIZED:
+                return getHttpResponse401(statusLine, header, url, dos);
+            default:
+                return getHttpResponse404(statusLine, header, url, dos);
         }
     }
 
-    private static HttpResponse getResponse(StatusCode statusCode, Map<String, String> result,
-            Map<String, String> model, DataOutputStream dos) throws IOException {
-        Logger logger = LoggerFactory.getLogger(HttpResponseFactory.class);
-        logger.info("statusCode : {}, result : {}", statusCode.toString(), result.toString());
-        byte[] body = ViewMaker.getView(result.get("url"), model);
-        List<String> splitResult = List.of(result.get("url").split("\\."));
-        int length = splitResult.size();
-        String extension = splitResult.get(length - 1);
-        switch (statusCode) {
-            case OK:
-                return new HttpResponse200(dos, ContentType.getContentType(extension), body);
-            case FOUND:
-                return new HttpResponse302(dos, result.get("url"));
-            case UNAUTHORIZED:
-                return new HttpResponse401(dos, ContentType.getContentType(extension), body);
-            default:
-                return new HttpResponse404(dos, ViewMaker.getNotFoundView());
+    private static String getContentType(String url) {
+        List<String> splitResult = List.of(url.split("\\."));
+        if (splitResult.size() == 1) {
+            return ContentType.DEFAULT.getType();
         }
+        String extension = splitResult.get(splitResult.size() - 1);
+        return ContentType.getContentType(extension);
+    }
+
+    private static NewHttpResponse getHttpResponse200(ResponseStatusLine statusLine,
+            ResponseHeader header, String url, DataOutputStream dos) {
+        ResponseBody body = getResponseBody(header, url);
+        return new NewHttpResponse(statusLine, header, body, dos);
+    }
+
+    private static NewHttpResponse getHttpResponse302(ResponseStatusLine statusLine,
+            ResponseHeader header, String url, DataOutputStream dos) {
+        ResponseBody body = new ResponseBody(ViewMaker.getView(url, new HashMap<>()));
+        header.addComponent("Location", url);
+        return new NewHttpResponse(statusLine, header, body, dos);
+    }
+
+    private static NewHttpResponse getHttpResponse401(ResponseStatusLine statusLine,
+            ResponseHeader header, String url, DataOutputStream dos) {
+        ResponseBody body = getResponseBody(header, url);
+        return new NewHttpResponse(statusLine, header, body, dos);
+    }
+
+    private static NewHttpResponse getHttpResponse404(ResponseStatusLine statusLine,
+            ResponseHeader header, String url, DataOutputStream dos) {
+        ResponseBody body = new ResponseBody(ViewMaker.getNotFoundView());
+        header.addComponent("Content-Type", getContentType(url));
+        header.addComponent("Content-Length", String.valueOf(body.getBodyLength()));
+        return new NewHttpResponse(statusLine, header, body, dos);
+    }
+
+    private static ResponseBody getResponseBody(ResponseHeader header, String url) {
+        ResponseBody body = new ResponseBody(ViewMaker.getView(url, new HashMap<>()));
+        header.addComponent("Content-Type", getContentType(url));
+        header.addComponent("Content-Length", String.valueOf(body.getBodyLength()));
+        return body;
     }
 }
