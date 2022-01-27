23cd8c5f55fcd9dffbe15b482e6dacc7f8627774
MELT <melt.ep00@gmail.com>
2022-01-27 18:12:48 +0900

    간단 웹 서버 4단계 구현 - 'Response' 에 Builder 패턴 도입

--- src/main/java/controller/response/Response.java
+++ src/main/java/controller/response/Response.java
@@ -2,6 +2,8 @@ package controller.response;
 
 import util.HttpStatus;
 
+import java.util.Map;
+
 /**
  * Created by melodist
  * Date: 2022-01-26 026
@@ -9,18 +11,63 @@ import util.HttpStatus;
  */
 public class Response {
     private HttpStatus httpStatus;
-    private String responseUrl;
-
-    public Response(HttpStatus httpStatus, String responseUrl) {
-        this.httpStatus = httpStatus;
-        this.responseUrl = responseUrl;
-    }
+    private Map<String, String> responseHeader;
+    private byte[] responseBody;
 
     public HttpStatus getHttpStatus() {
         return httpStatus;
     }
 
-    public String getResponseUrl() {
-        return responseUrl;
+    public Map<String, String> getResponseHeader() {
+        return responseHeader;
+    }
+
+    public byte[] getResponseBody() {
+        return responseBody;
+    }
+
+    public static class Builder {
+        private HttpStatus httpStatus;
+        private Map<String, String> responseHeader;
+        private byte[] responseBody;
+
+        public Builder() {
+
+        }
+
+        public Builder ok() {
+            httpStatus = HttpStatus.OK;
+            return this;
+        }
+
+        public Builder redirect() {
+            httpStatus = HttpStatus.REDIRECT;
+            return this;
+        }
+
+        public Builder notFound() {
+            httpStatus = HttpStatus.NOT_FOUND;
+            return this;
+        }
+
+        public Builder headers(Map<String, String> headers) {
+            this.responseHeader = headers;
+            return this;
+        }
+
+        public Builder body(byte[] responseBody) {
+            this.responseBody = responseBody;
+            return this;
+        }
+
+        public Response build() {
+            return new Response(this);
+        }
+    }
+
+    public Response(Builder responseBuilder) {
+        httpStatus = responseBuilder.httpStatus;
+        responseHeader = responseBuilder.responseHeader;
+        responseBody = responseBuilder.responseBody;
     }
 }
