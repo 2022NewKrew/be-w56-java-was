abe2ef715bdf32f61b3eda4696f3e4fcf820b20d
kimdaeseon <kimdaeseon@khu.ac.kr>
2022-01-27 14:04:03 +0900

    HttpMethod 열거형 생성 및 ControllerType 수정
    -- 경로만 사용해서 매칭 -> Method도 사용해서 매칭
    ---- (/user/create -> UserCreateController, POST /user/crate -> UserCreateController)

--- src/main/java/webserver/ControllerType.java
+++ src/main/java/webserver/ControllerType.java
@@ -3,6 +3,8 @@ package webserver;
 import controller.Controller;
 import controller.StaticFileController;
 import controller.UserCreateController;
+import java.util.Collections;
+import java.util.EnumMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import org.slf4j.Logger;
@@ -10,27 +12,40 @@ import org.slf4j.LoggerFactory;
 
 public enum ControllerType {
 
-    STATIC_FILE_CONTROLLER("", StaticFileController.getInstance()),
-    USER_CREATE_CONTROLLER("/user/create", UserCreateController.getInstance());
+    STATIC_FILE_CONTROLLER(HttpMethod.GET, "", StaticFileController.getInstance()),
+    USER_CREATE_CONTROLLER(HttpMethod.POST, "/user/create", UserCreateController.getInstance());
     public static final Logger log = LoggerFactory.getLogger(ControllerType.class);
+    public static final Map<HttpMethod, Map<String, Controller>> methodMap;
     public static final Map<String, Controller> controllerMap = new ConcurrentHashMap<>();
 
     static {
+        methodMap = Collections.synchronizedMap(new EnumMap<>(HttpMethod.class));
+
         for (ControllerType controllerType : ControllerType.values()) {
-            controllerMap.put(controllerType.getPath(), controllerType.getController());
+            methodMap
+                    .get(controllerType.getMethod())
+                    .put(controllerType.getPath(), controllerType.getController());
         }
     }
 
-    public static Controller getControllerType(String path) {
-        return controllerMap.getOrDefault(path, STATIC_FILE_CONTROLLER.getController());
+    public static Controller getControllerType(HttpMethod httpMethod, String path) {
+        return methodMap
+                .get(httpMethod)
+                .get(path);
     }
 
+    private final HttpMethod method;
     private final String path;
     private final Controller controller;
 
-    ControllerType(String path, Controller controller) {
+    ControllerType(HttpMethod method, String path, Controller controller) {
+        this.method = method;
         this.path = path;
         this.controller = controller;
+    }
+
+    public HttpMethod getMethod() {
+        return method;
     }
 
     public String getPath() {
