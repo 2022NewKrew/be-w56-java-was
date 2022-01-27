9eecf7d6f30ae094e56e4dd2d70e5b5551c31402
kimdaeseon <kimdaeseon@khu.ac.kr>
2022-01-27 14:18:21 +0900

    ControllerType 실수 수정
    -- enumMap을 만들고 value에 Map을 넣어주지 않았던 실수 수정
    -- default로 static file을 리턴하도록 수정 (getControllerType 메서드)

--- src/main/java/webserver/ControllerType.java
+++ src/main/java/webserver/ControllerType.java
@@ -3,27 +3,25 @@ package webserver;
 import controller.Controller;
 import controller.StaticFileController;
 import controller.UserCreateController;
-import controller.UserLoginController;
 import java.util.Collections;
 import java.util.EnumMap;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public enum ControllerType {
 
     STATIC_FILE_CONTROLLER(HttpMethod.GET, "", StaticFileController.getInstance()),
-    USER_CREATE_CONTROLLER(HttpMethod.POST, "/user/create", UserCreateController.getInstance()),
-    USER_LOGIN_CONTROLLER(HttpMethod.POST, "/user/login", UserLoginController.getInstance());
+    USER_CREATE_CONTROLLER(HttpMethod.POST, "/user/create", UserCreateController.getInstance());
 
     public static final Map<HttpMethod, Map<String, Controller>> methodMap;
 
     static {
         methodMap = Collections.synchronizedMap(new EnumMap<>(HttpMethod.class));
 
-        for (HttpMethod method : HttpMethod.values()) {
-            methodMap.put(method, new HashMap<>());
+        for(HttpMethod method : HttpMethod.values()){
+            methodMap.put(method, new ConcurrentHashMap<>());
         }
 
         for (ControllerType controllerType : ControllerType.values()) {
@@ -34,9 +32,6 @@ public enum ControllerType {
     }
 
     public static Controller getControllerType(HttpMethod httpMethod, String path) {
-        Logger log = LoggerFactory.getLogger(ControllerType.class);
-        log.info("method : {}, path : {}", httpMethod, path);
-        log.info("methodMap : {}", methodMap.toString());
         return methodMap
                 .get(httpMethod)
                 .getOrDefault(path, STATIC_FILE_CONTROLLER.getController());
