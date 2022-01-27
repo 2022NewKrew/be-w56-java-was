711f643a59a94c6ce499b2d242789310398c8571
rogers.t <rogers.t@kakaocorp.com>
2022-01-27 16:46:30 +0900

    refactoring : lombok의 생성자로 변경, 예외 안던지는 부분 throw 제거

--- src/main/java/util/response/HttpResponseStatus.java
+++ src/main/java/util/response/HttpResponseStatus.java
@@ -1,17 +1,14 @@
 package util.response;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public enum HttpResponseStatus {
     SUCCESS(200, "OK"), REDIRECT(302, "Found"),
     NOT_FOUND(404, "Not Found"), INTERNAL_ERROR(500, "Internal Server Error");
 
     private final int statusCode;
     private final String text;
-
-    HttpResponseStatus(int statusCode, String text) {
-        this.statusCode = statusCode;
-        this.text = text;
-    }
 }
