9427069b283df7a2ca332ad2640bee12b62c2cff
kimdaeseon <kimdaeseon@khu.ac.kr>
2022-01-27 13:50:28 +0900

    RequestBody 에서 body로 넘어온 데이터를 Map의 형태로가지도록 구현
    -- 받은 body가 없는경우 (rawData == null) 빈 Map을 가짐
    -- 받은 body가 존재하는 경우 (rawData != null) Map에 utf-8로 디코딩 해서 채움

--- src/main/java/http/request/RequestBody.java
+++ src/main/java/http/request/RequestBody.java
@@ -1,5 +1,10 @@
 package http.request;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -7,10 +12,31 @@ public class RequestBody {
 
     Logger logger = LoggerFactory.getLogger(RequestBody.class);
 
-    private final String body;
+    private final Map<String, String> bodyData;
 
     public RequestBody(String rawBody) {
-        logger.info(rawBody);
-        this.body = rawBody;
+        this.bodyData = new HashMap<>();
+        if (rawBody == null) {
+            return;
+        }
+        setBodyData(rawBody);
+        logger.info(bodyData.toString());
+    }
+
+    private void setBodyData(String rawBody) {
+        List<String> dataLine = List.of(rawBody.split("&"));
+        List<String> components;
+        String key;
+        String value;
+        try {
+            for (String line : dataLine) {
+                components = List.of(line.split("="));
+                key = URLDecoder.decode(components.get(0), "utf-8");
+                value = URLDecoder.decode(components.get(1), "utf-8");
+                bodyData.put(key, value);
+            }
+        } catch (UnsupportedEncodingException exception) {
+            throw new IllegalArgumentException("body decoding에 실패하였습니다");
+        }
     }
 }
