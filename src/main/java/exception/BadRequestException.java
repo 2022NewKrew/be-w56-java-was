b1b60adb100e0a4d3337bdfd0f17566b86d1363a
kimdaeseon <kimdaeseon@khu.ac.kr>
2022-01-27 16:39:12 +0900

    unchecked Exception BadRequestException 추가

---
+++ src/main/java/exception/BadRequestException.java
@@ -0,0 +1,11 @@
+package exception;
+
+public class BadRequestException extends RuntimeException{
+    public BadRequestException() {
+        super();
+    }
+
+    public BadRequestException(String msg) {
+        super(msg);
+    }
+}
