3359aeae716073e9c1b357f08b381f6441f32213
MELT <melt.ep00@gmail.com>
2022-01-27 18:17:19 +0900

    간단 웹 서버 4단계 구현 - 'Response' 구조 변경에 따른 수정

--- src/main/java/controller/UserCreateController.java
+++ src/main/java/controller/UserCreateController.java
@@ -4,8 +4,6 @@ import controller.request.Request;
 import controller.response.Response;
 import db.DataBase;
 import model.User;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import util.HttpStatus;
 
 import java.util.HashMap;
@@ -17,17 +15,14 @@ import java.util.Map;
  * Time: 오후 4:22
  */
 public class UserCreateController implements WebController{
-    private static final Logger log = LoggerFactory.getLogger(UserCreateController.class);
 
     @Override
     public Response process(Request request) {
-        User user = new User(request.getBody("userId"),
-                request.getBody("password"),
-                request.getBody("name"),
-                request.getBody("email"));
+        User user = new User(request.getQueryStringParams("userId"),
+                request.getQueryStringParams("password"),
+                request.getQueryStringParams("name"),
+                request.getQueryStringParams("email"));
         DataBase.addUser(user);
-
-        log.debug("user: {}", user);
 
         Map<String, String> headers = new HashMap<>();
         headers.put("Location", "/index.html");
