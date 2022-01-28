7a346cce7cd34690ad901a21a7db1e9c9eeef494
MELT <melt.ep00@gmail.com>
2022-01-28 23:22:05 +0900

    간단 웹 서버 4단계 구현 - 불필요 import 제거

--- src/main/java/controller/UserCreateController.java
+++ src/main/java/controller/UserCreateController.java
@@ -2,8 +2,6 @@ package controller;
 
 import controller.request.Request;
 import controller.response.Response;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import service.UserService;
 
 import java.util.HashMap;
@@ -15,8 +13,6 @@ import java.util.Map;
  * Time: 오후 4:22
  */
 public class UserCreateController implements WebController{
-    private static final Logger log = LoggerFactory.getLogger(UserCreateController.class);
-
     @Override
     public Response process(Request request) {
         String userId = request.getBody("userId");
