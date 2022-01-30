71c0667c4c0a7e8a1da9a5c77cc16b217b90b393
kimdaeseon <kimdaeseon@khu.ac.kr>
2022-01-30 11:02:50 +0900

    RequestBody, String을 파라미터로 받아 생성하는 정적 팩토리 메서드 추가

--- src/main/java/http/request/RequestBody.java
+++ src/main/java/http/request/RequestBody.java
@@ -6,33 +6,36 @@ import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class RequestBody {
 
     private final Map<String, String> bodyData;
 
-    public RequestBody(String rawBody) {
-        this.bodyData = new HashMap<>();
-        if (rawBody == null) {
-            return;
-        }
-        setBodyData(rawBody);
+    public RequestBody(Map<String, String> bodyData) {
+        this.bodyData = bodyData;
     }
 
-    private void setBodyData(String rawBody) {
-        List<String> dataLine = List.of(rawBody.split("&"));
+    public static RequestBody stringToRequestBody(String body) {
+        if (body.isEmpty()) {
+            return new RequestBody(new HashMap<>());
+        }
+        List<String> bodyLines = List.of(body.split("&"));
+        return new RequestBody(getBodyData(bodyLines));
+    }
+
+    private static Map<String, String> getBodyData(List<String> bodyLines) {
+        Map<String, String> result = new HashMap<>();
         List<String> components;
         String key;
         String value;
         try {
-            for (String line : dataLine) {
+            for (String line : bodyLines) {
                 components = List.of(line.split("="));
                 key = URLDecoder.decode(components.get(0), StandardCharsets.UTF_8);
                 value = URLDecoder.decode(components.get(1), StandardCharsets.UTF_8);
-                bodyData.put(key, value);
+                result.put(key, value);
             }
+            return result;
         } catch (Exception exception) {
             throw new BadRequestException("body decoding에 실패하였습니다");
         }
