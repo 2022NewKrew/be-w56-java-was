b539d8aa4f17ddb0c5ffd9d81edd9d88e4fa3f5b
ChaeHyeon <cohys7@gmail.com>
2022-01-30 16:11:30 +0900

    url decoding 추가

--- src/main/java/http/Parameters.java
+++ src/main/java/http/Parameters.java
@@ -4,6 +4,8 @@ import com.google.common.base.Strings;
 import util.HttpRequestParser;
 import util.Pair;
 
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -28,7 +30,7 @@ public class Parameters {
             return new Parameters();
         }
 
-        String[] tokens = parameters.split(DELIMITER);
+        String[] tokens = URLDecoder.decode(parameters, StandardCharsets.UTF_8).split(DELIMITER);
         Map<String, String> result = Arrays.stream(tokens)
                 .map(token -> HttpRequestParser.getKeyValue(token, "="))
                 .filter(Objects::nonNull)
