6889aa736cb956646f410d9ab0315336d2c58c2c
charlie.p1 <charlie.p1@kakaocorp.com>
2022-02-03 16:41:26 +0900

    Refactor UserController

--- src/main/java/com/kakao/webserver/controller/UserController.java
+++ src/main/java/com/kakao/webserver/controller/UserController.java
@@ -11,23 +11,30 @@ import com.kakao.model.User;
 import com.kakao.service.UserService;
 
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
+import java.util.function.Function;
 
 import static com.kakao.webserver.WebServerConfig.DEFAULT_HTTP_VERSION;
 
 public class UserController implements HttpController {
     private final UserService userService = UserService.getInstance();
-    private final Set<HttpRoute> routeSet = Set.of(
-            new HttpRoute(HttpMethod.POST, "/user/create")
+    private final Map<HttpRoute, Function<HttpRequest, HttpResponse>> routeMap = Map.ofEntries(
+            Map.entry(new HttpRoute(HttpMethod.POST, "/user/create"), this::createUser)
     );
 
     @Override
     public boolean isValidRoute(HttpRoute route) {
-        return routeSet.contains(route);
+        return routeMap.containsKey(route);
     }
 
     @Override
     public HttpResponse handleRequest(HttpRequest request) {
+        HttpRoute route = new HttpRoute(request.getMethod(), request.getUrl().getPath());
+        return routeMap.get(route)
+                .apply(request);
+    }
+
+    private HttpResponse createUser(HttpRequest request) {
         User user = buildUser(request);
         userService.addUser(user);
 
