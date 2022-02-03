4d6548abf57edf55e39d09471703d90241a55d22
kenny.park <kenny.park@kakaocorp.com>
2022-02-03 19:04:34 +0900

    ReflectionUtils 내 필요없는 메소드 삭제

--- src/main/java/util/ReflectionUtils.java
+++ src/main/java/util/ReflectionUtils.java
@@ -3,20 +3,10 @@ package util;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class ReflectionUtils {
-    public static List<Method> findAllMethods(String packageName){
-        List<Class> classes = findAllClasses(packageName);
-        List<Method> methods = new ArrayList<>();
-        classes.forEach(item -> methods.addAll(Arrays.asList(item.getDeclaredMethods())));
-        return methods;
-    }
-
     public static List<Class> findAllClasses(String packageName) {
         InputStream stream = ClassLoader.getSystemClassLoader()
                 .getResourceAsStream(packageName.replaceAll("[.]", "/"));
