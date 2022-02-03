c7c1d1d9a47233642ee148a4312a64ffa14845b3
kimdaeseon <kimdaeseon@khu.ac.kr>
2022-02-03 10:53:43 +0900

    protocol 오타 수정 , -> .

--- src/main/java/util/Constant.java
+++ src/main/java/util/Constant.java
@@ -3,5 +3,5 @@ package util;
 public class Constant {
 
     public static final String lineBreak = "\r\n";
-    public static final String protocol = "HTTP/1,1";
+    public static final String protocol = "HTTP/1.1";
 }
