013fc688e09e445e9866d064aff60859a082e8e1
kimdaeseon <kimdaeseon@khu.ac.kr>
2022-02-03 11:11:25 +0900

    불필요한 import 삭제

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -11,8 +11,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
-import java.util.HashMap;
-import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
