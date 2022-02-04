842e5cf427bb115c334ef299519fbc8d1a12a456
kimdaeseon@khu.ac.kr <kimdaeseon@khu.ac.kr>
2022-02-04 12:03:16 +0900

    ResponseHeader 주요 키 상수화

--- src/main/java/http/response/ResponseHeader.java
+++ src/main/java/http/response/ResponseHeader.java
@@ -8,6 +8,14 @@ import util.MapUtil;
 
 public class ResponseHeader {
 
+    public static final String CONTENT_LENGTH = "Content-Length";
+    public static final String CONTENT_TYPE = "Content-Type";
+    public static final String LOCATION = "Location";
+    public static final String SET_COOKIE = "Set-Cookie";
+    public static final String EXTENSION_SEPARATOR = "\\.";
+    public static final String HEADER_LINE_SEPARATOR = ": ";
+    public static final String COOKIE_SEPARATOR = "=";
+
     private final Map<String, String> components;
     private final Map<String, String> cookies;
 
@@ -21,25 +29,25 @@ public class ResponseHeader {
     }
 
     public void addContentType(String url) {
-        List<String> splitResult = List.of(url.split("\\."));
+        List<String> splitResult = List.of(url.split(EXTENSION_SEPARATOR));
         if (splitResult.size() == 1) {
-            addComponent("Content-Type", ContentType.DEFAULT.getType());
+            addComponent(CONTENT_TYPE, ContentType.DEFAULT.getType());
             return;
         }
         String extension = splitResult.get(splitResult.size() - 1);
-        addComponent("Content-Type", ContentType.getContentType(extension));
+        addComponent(CONTENT_TYPE, ContentType.getContentType(extension));
     }
 
     public void addContentType(ContentType type) {
-        addComponent("Content-Type", type.getType());
+        addComponent(CONTENT_TYPE, type.getType());
     }
 
     public void addContentLength(int length) {
-        addComponent("Content-Length", String.valueOf(length));
+        addComponent(CONTENT_LENGTH, String.valueOf(length));
     }
 
     public void addLocation(String location) {
-        addComponent("Location", location);
+        addComponent(LOCATION, location);
     }
 
     public void addCookie(Map<String, String> cookie) {
@@ -57,13 +65,18 @@ public class ResponseHeader {
         StringBuilder result = new StringBuilder();
 
         for (Entry<String, String> entry : components.entrySet()) {
-            result.append(entry.getKey()).append(": ").append(entry.getValue())
+            result.append(entry.getKey()).append(HEADER_LINE_SEPARATOR).append(entry.getValue())
                     .append(Constant.lineBreak);
         }
 
         for (Entry<String, String> entry : cookies.entrySet()) {
-            result.append("Set-Cookie").append(": ").append(entry.getKey()).append("=")
-                    .append(entry.getValue()).append(Constant.lineBreak);
+            result
+                    .append(SET_COOKIE)
+                    .append(HEADER_LINE_SEPARATOR)
+                    .append(entry.getKey())
+                    .append(COOKIE_SEPARATOR)
+                    .append(entry.getValue())
+                    .append(Constant.lineBreak);
         }
 
         return result.toString();
