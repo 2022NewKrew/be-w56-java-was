833ff956eef2771d3bea085c41e17c22b54012fd
unfunhy <unfunhy@gmail.com>
2022-02-04 17:01:09 +0900

    웹 서버 4단계 구현 - 로그인 및 쿠키 세팅 기능 구현

--- src/main/java/mvc/service/UserService.java
+++ src/main/java/mvc/service/UserService.java
@@ -14,8 +14,26 @@ public class UserService {
                 .email(dto.getEmail())
                 .build();
         DataBase.addUser(newUser);
-        /* for test ... */
-        System.out.println(DataBase.findAll());
         return dto;
+    }
+
+    public UserDto login(UserDto dto) {
+        var dbUser = DataBase.findUserById(dto.getUserId());
+        if (dbUser == null) {
+            throw new IllegalArgumentException("존재하지 않는 회원입니다.");
+        }
+        validatePassword(dbUser, dto);
+        return UserDto.builder()
+                .userId(dbUser.getUserId())
+                .password(dbUser.getPassword())
+                .name(dbUser.getName())
+                .email(dbUser.getEmail())
+                .build();
+    }
+
+    private void validatePassword(User dbUser, UserDto requestUser) {
+        if (!dbUser.getPassword().equals(requestUser.getPassword())) {
+            throw new IllegalArgumentException("비밀번호가 올바르지 않습니다");
+        }
     }
 }
