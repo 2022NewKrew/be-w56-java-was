23d7bb591fa2cebed59f66f2a38779b13b4ae475
danny.an <danny.an@kakaocorp.com>
2022-02-08 00:34:38 +0900

    MySql DB 연동 (User Insert)

--- src/main/java/db/DataBase.java
+++ src/main/java/db/DataBase.java
@@ -2,13 +2,19 @@ package db;
 
 import java.util.Collection;
 import java.util.Map;
+import java.sql.*;
 
 import com.google.common.collect.Maps;
 
 import model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import util.DataBaseUtils;
 
 public class DataBase {
+
     private static Map<String, User> users = Maps.newHashMap();
+    private static Logger log = LoggerFactory.getLogger(DataBase.class);
 
     public static void addUser(User user) {
         users.put(user.getUserId(), user);
@@ -20,5 +26,20 @@ public class DataBase {
 
     public static Collection<User> findAll() {
         return users.values();
+    }
+
+    public static void add(User user) throws SQLException {
+        Connection connection = DataBaseUtils.connectDB();
+
+        String sql = "INSERT INTO user (userId, password, name, email) VALUES (?, ?, ?, ?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setString(1, user.getUserId());
+        preparedStatement.setString(2, user.getPassword());
+        preparedStatement.setString(3, user.getName());
+        preparedStatement.setString(4, user.getEmail());
+        preparedStatement.executeUpdate();
+
+        preparedStatement.close();
+        connection.close();
     }
 }
--- src/main/java/util/DataBaseUtils.java
+++ src/main/java/util/DataBaseUtils.java
@@ -1,13 +1,21 @@
 package util;
 
 import db.DataBase;
-import model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.Collection;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.IntStream;
 
 public class DataBaseUtils {
+
+    private static final Properties properties = new Properties();
+    private static final Logger log = LoggerFactory.getLogger(DataBaseUtils.class);
 
     public static String setUserTable() {
         StringBuilder users = new StringBuilder();
@@ -19,5 +27,22 @@ public class DataBaseUtils {
             users.append("</tr>\n");
         });
         return users.toString();
+    }
+
+    public static Connection connectDB() {
+        try {
+            properties.load(new FileInputStream("local.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            String url = properties.getProperty("DB_HOST");
+            String id = properties.getProperty("DB_NAME");
+            String password = properties.getProperty("DB_PASSWORD");
+            return DriverManager.getConnection(url, id, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e.getMessage());
+        }
     }
 }
