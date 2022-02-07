feca5e0acf09567d3e39a12909ad9ea4d5d678a3
jin.jang <jin.jang@kakaocorp.com>
2022-02-07 10:56:56 +0900

    [fix] 포트 변환 로직 수정

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -8,15 +8,13 @@ import org.slf4j.LoggerFactory;
 public class WebServer {
 
     private static final Logger log = LoggerFactory.getLogger(WebServer.class);
+    private static final int MIN_PORT = 0;
+    private static final int MAX_PORT = 65535;
     private static final int DEFAULT_PORT = 8080;
 
     public static void main(String[] args) throws Exception {
-        int port = 0;
-        if (args == null || args.length == 0) {
-            port = DEFAULT_PORT;
-        } else {
-            port = Integer.parseInt(args[0]);
-        }
+        int port;
+        port = transformPort(args);
 
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             log.info("Web Application Server started {} port.", port);
@@ -25,6 +23,19 @@ public class WebServer {
                 RequestHandler requestHandler = new RequestHandler(connection);
                 requestHandler.start();
             }
+        }
+    }
+
+    private static int transformPort(String[] args) {
+        int port;
+        try {
+            port = Integer.parseInt(args[0]);
+            if (port < MIN_PORT || port > MAX_PORT) {
+                port = DEFAULT_PORT;
+            }
+            return port;
+        } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+            return DEFAULT_PORT;
         }
     }
 }
