64885dfef066fc86fea9e83f31a1db8e43e8c5c9
david.dh <david.dh@kakaocorp.com>
2022-02-07 18:36:20 +0900

    http 상태 코드 401 Unauthorized 추가

--- src/main/java/util/HttpStatus.java
+++ src/main/java/util/HttpStatus.java
@@ -4,7 +4,8 @@ public enum HttpStatus {
     OK("200 OK"),
     FOUND("302 Found"),
     BAD_REQUEST("400 Bad Request"),
-    NOT_FOUND("404 Not Found");
+    NOT_FOUND("404 Not Found"),
+    UNAUTHORIZED("401 Unauthorized");
 
     final String httpStatus;
 
