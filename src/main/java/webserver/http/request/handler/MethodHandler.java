5c058d6a5f1da8b9e2d3d4641197cf2398d001f2
champ.ion <champ.ion@kakaocorp.com>
2022-02-07 10:52:25 +0900

    refactor(웹 서버 4단계 피드백(PR#221)): Method Handler 통합
    Map<Pair<Method,URI>, Controller>를 통해 GET/POST Method Handler를
    하나의 Method Handler로 통합

--- src/main/java/webserver/http/request/handler/MethodHandler.java
+++ src/main/java/webserver/http/request/handler/MethodHandler.java
@@ -1,12 +1,84 @@
 package webserver.http.request.handler;
 
+import Controller.Controller;
+import Controller.LoginController;
+import Controller.SignUpUserController;
+import Controller.StaticFileController;
 import java.io.IOException;
-import webserver.http.request.exceptions.PageNotFoundException;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 import webserver.http.request.HttpRequest;
+import webserver.http.request.Method;
+import webserver.http.request.exceptions.PageNotFoundException;
 import webserver.http.response.HttpResponse;
 
-public interface MethodHandler {
+public class MethodHandler {
 
-    void handle(HttpRequest request, HttpResponse response)
-        throws IOException, PageNotFoundException;
+    private static final Map<Pair, Controller> controllerMap = new HashMap<>();
+
+    static {
+        controllerMap.put(new Pair(Method.POST, "/user/create"), new SignUpUserController());
+        controllerMap.put(new Pair(Method.POST, "/user/login"), new LoginController());
+    }
+
+    private final StaticFileController staticFileController = new StaticFileController();
+
+    public void handle(HttpRequest request, HttpResponse response)
+        throws IOException, PageNotFoundException {
+        if (!Method.contain(request.getMethod())) {
+            response.setStatusCode(HttpURLConnection.HTTP_BAD_REQUEST);
+            throw new PageNotFoundException();
+        }
+
+        Method method = request.getMethod();
+        URI uri = request.getUri();
+        Controller controller = getController(method, uri);
+
+        if (controller != null) {
+            controller.process(request, response);
+            return;
+        }
+
+        if (request.getMethod() == Method.POST) {
+            throw new PageNotFoundException();
+        }
+
+        staticFileController.process(request, response);
+    }
+
+    private Controller getController(Method method, URI uri) {
+        Pair pair = new Pair(method, uri.getPath());
+        return controllerMap.get(pair);
+    }
+
+    private static class Pair {
+
+        private final Method method;
+        private final String uri;
+
+        public Pair(Method method, String uri) {
+            this.method = method;
+            this.uri = uri;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            Pair pair = (Pair) o;
+            return method == pair.method && uri.equals(pair.uri);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(method, uri);
+        }
+    }
 }
