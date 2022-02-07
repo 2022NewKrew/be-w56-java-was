68b677f4165dcd06478564e4cf17c296584c1f6e
mimseong <parkseongmin98@gmail.com>
2022-02-07 14:02:39 +0900

    feat: HttpMethod enum으로 변경

--- src/main/java/model/StartLine.java
+++ src/main/java/model/StartLine.java
@@ -8,12 +8,12 @@ public class StartLine {
         return new StartLine(token[0], token[1], token[2]);
     }
 
-    private final String httpMethod; // TODO - 메소드 같은 경우는 enum으로 처리해도 괜찮을 것 같음
+    private final HttpMethod httpMethod;
     private final String requestTarget;
     private final String httpVersion;
 
     private StartLine(String httpMethod, String requestTarget, String httpVersion) {
-        this.httpMethod = httpMethod;
+        this.httpMethod = HttpMethod.fromString(httpMethod);
         this.requestTarget = requestTarget;
         this.httpVersion = httpVersion;
     }
