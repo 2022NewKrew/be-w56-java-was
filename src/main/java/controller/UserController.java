cfc3a1d30c6bf1a4eec0e80ed33df7b31f7393b8
don-garu <bryan.ik@kakaocorp.com>
2022-02-07 14:17:06 +0900

    FEAT : 사용자 리스트 조회에 대한 Get 요청 매핑 컨트롤러 구현

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,16 +1,25 @@
 package controller;
 
+import controller.exception.ControllerMismatchException;
+import controller.util.UserHtmlUtils;
 import domain.user.dto.UserCreate;
+import domain.user.dto.UserInfo;
 import domain.user.service.UserService;
 import http.HttpHeader;
 import http.HttpRequest;
 import http.HttpResponse;
 import http.HttpStatus;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 import java.util.Map;
+import util.HttpRequestUtils;
 
 public class UserController implements Controller {
 
+    private static final String USER_LIST_PATH = "/user/list";
     private static final String USER_CREATE_PATH = "/user/create";
+
+    private static final String USER_LOGIN_URL = "/user/login.html";
     private static final String USER_CREATE_REDIRECT_URL = "/index.html";
 
     private final UserService userService;
@@ -24,9 +33,38 @@ public class UserController implements Controller {
     }
 
     @Override
+    public HttpResponse doGet(HttpRequest request) {
+        String path = request.getPath();
+        if (!path.equals(USER_LIST_PATH)) {
+            throw new ControllerMismatchException("Request Path :: " + request.getPath());
+        }
+
+        Map<String, String> cookies = HttpRequestUtils.parseCookies(request.getHeader("Cookie"));
+        String authentication = cookies.getOrDefault("logined", "false");
+
+        if (!Boolean.parseBoolean(authentication)) {
+            HttpHeader httpHeader = HttpHeader.of(Map.of("Location", USER_LOGIN_URL));
+
+            return HttpResponse.builder()
+                .status(HttpStatus.FOUND)
+                .header(httpHeader)
+                .build();
+        }
+
+        List<UserInfo> userInfos = userService.readAll();
+
+        String body = UserHtmlUtils.generateUsersHtml(userInfos);
+
+        return HttpResponse.builder()
+            .status(HttpStatus.OK)
+            .responseBody(body.getBytes(StandardCharsets.UTF_8))
+            .build();
+    }
+
+    @Override
     public HttpResponse doPost(HttpRequest request) {
         String path = request.getPath();
-        if (!USER_CREATE_PATH.equals(path)) {
+        if (!path.equals(USER_CREATE_PATH)) {
             return badRequest();
         }
 
