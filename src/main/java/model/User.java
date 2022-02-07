4354d9678e48e9a04acbb66a299edd7b0a177ca7
Jungyu Choi <devwithpug@kakao.com>
2022-02-07 11:30:17 +0900

    step-7: User 클래스 id 프로퍼티 추가 & getter setter 추가

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,16 +1,33 @@
 package model;
 
 public class User {
+    private Long id;
     private String userId;
-    private String password;
     private String name;
     private String email;
+    private String password;
 
-    public User(String userId, String password, String name, String email) {
+    public User(String userId, String name, String email, String password) {
         this.userId = userId;
-        this.password = password;
         this.name = name;
         this.email = email;
+        this.password = password;
+    }
+
+    public User(Long id, String userId, String name, String email, String password) {
+        this.id = id;
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getUserId() {
@@ -25,12 +42,28 @@ public class User {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getEmail() {
         return email;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     @Override
     public String toString() {
-        return "User [userId=" + userId + ", password=" + password + ", name=" + name + ", email=" + email + "]";
+        return "User [userId=" + userId + ", name=" + name + ", email=" + email + "]";
     }
 }
