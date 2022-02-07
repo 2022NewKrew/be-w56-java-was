c356456a829c443e3fe214a381875a942e5dbb6c
Hal-ws <jwsnada@gmail.com>
2022-02-08 01:05:36 +0900

    step3

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,19 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
 
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
+import util.IOUtils;
+
+import static util.HttpRequestUtils.getUrlExtension;
+import static util.HttpRequestUtils.parseRequestLine;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -24,10 +30,46 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            String requestLine = br.readLine();
+
+            HttpRequestUtils.Pair urlPair = parseRequestLine(requestLine);
+            String url = urlPair.getValue();
+            Map<String, String> headers = new HashMap<String, String>();
+            while(!requestLine.equals("")) {
+                if (requestLine == null) {
+                    break;
+                }
+                requestLine = br.readLine();
+                String[] headerTokens = requestLine.split(": ");
+                if (headerTokens.length == 2) {
+                    headers.put(headerTokens[0], headerTokens[1]);
+                }
+            }
+
+            if (url.startsWith("/user/create")) {
+                String requestBody = IOUtils.readData(br, Integer.parseInt(headers.get("Content-Length")));
+                Map<String, String> params = HttpRequestUtils.parseQueryString(requestBody);
+                User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
+                DataOutputStream dos = new DataOutputStream(out);
+                response302Header(dos);
+            }
+            else {
+                DataOutputStream dos = new DataOutputStream(out);
+                byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+                response200Header(dos, body.length);
+                responseBody(dos, body);
+            }
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void response302Header(DataOutputStream dos) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 Found \r\n");
+            dos.writeBytes("Location: /index.html \r\n");
+            dos.writeBytes("\r\n");
         } catch (IOException e) {
             log.error(e.getMessage());
         }
