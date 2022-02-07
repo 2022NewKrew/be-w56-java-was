fc1fb184fa06417d751cce2b66cf0d73a09d649d
chiveloper <wodlr2007@naver.com>
2022-02-07 17:11:36 +0900

    [feature] RequestHandler 분리
    - RequestHandler의 복잡성을 줄이기 위해 분리

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,13 +3,9 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 
-import domain.User;
 import network.HttpRequest;
-import network.HttpResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import util.HttpRequestUtils;
-import util.MyUtill;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -27,22 +23,14 @@ public class RequestHandler extends Thread {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
-            String headerLine = bufferedReader.readLine();
-            String query = headerLine.split(" ")[1];
 
             HttpRequest httpRequest = new HttpRequest(bufferedReader);
 
-            ResponseHandler responseHandler = new ResponseHandler(httpRequest);
+            ResponseHandler responseHandler = new ResponseHandler(httpRequest, out);
             responseHandler.run();
 
-            if(httpRequest.getQueryString() != null){
-                User user = new User(httpRequest.getQueryString());
-                return;
-            }
-            HttpResponse.handleHtmlResponse(httpRequest.getPath(), out);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
     }
-
 }
