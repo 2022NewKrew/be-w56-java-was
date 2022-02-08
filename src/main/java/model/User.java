bb98d6e7ba499fe66e96e347a4d5517cee6b8db9
keith.lee <keith.lee@kakaocorp.com>
2022-02-08 15:11:39 +0900

    DB 연결 구현

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -16,6 +16,20 @@ public class User {
         this.email = email;
     }
 
+    public User(Builder builder) {
+        if (builder.userId == null || builder.password == null || builder.name == null || builder.email == null) {
+            throw new IllegalArgumentException("IllegalArgumentException");
+        }
+        this.userId = builder.userId;
+        this.password = builder.password;
+        this.name = builder.name;
+        this.email = builder.email;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
     public String getUserId() {
         return userId;
     }
@@ -35,5 +49,34 @@ public class User {
     @Override
     public String toString() {
         return "User [userId=" + userId + ", password=" + password + ", name=" + name + ", email=" + email + "]";
+    }
+
+    public static class Builder {
+        private String userId;
+        private String password;
+        private String name;
+        private String email;
+
+        public Builder userId(String userId) {
+            this.userId = userId;
+            return this;
+        }
+
+        public Builder password(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public User build() { return new User(this); }
     }
 }
--- src/main/java/util/DBUtils.java
+++ src/main/java/util/DBUtils.java
@@ -1,2 +1,11 @@
-package util;public class DBUtils {
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtils {
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DBConfig.url, DBConfig.userName, DBConfig.password);
+    }
 }
