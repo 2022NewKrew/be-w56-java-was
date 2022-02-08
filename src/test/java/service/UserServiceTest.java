9a42032a6a14567be3ff5c87929c209cdee8f9e7
yuchanleeme <yuchanlee.me@gmail.com>
2022-02-08 16:20:35 +0900

    test: 유저 회원가입 테스트 코드 수정
    - user/login 으로 되어있던것 -> user/create로 수정
    - 바로 signUp 호출하는게 아니라 RequestController 통하게 변경

--- src/test/java/service/UserServiceTest.java
+++ src/test/java/service/UserServiceTest.java
@@ -2,10 +2,12 @@ package service;
 
 import db.DataBase;
 import model.User;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import util.HttpRequestUtils;
+import web.controller.RequestController;
 import web.http.request.HttpRequest;
 import web.http.response.HttpResponse;
 import web.http.response.HttpStatus;
@@ -15,50 +17,12 @@ import java.io.IOException;
 import java.io.StringReader;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.*;
 
 @DisplayName("유저 관련 테스트")
 class UserServiceTest {
-
-    @ParameterizedTest
-    @DisplayName("HTTP GET 요청 테스트")
-    @MethodSource("getHttpRequestString")
-    public void HTTP_요청_확인_테스트(String request) throws IOException {
-        // given
-        StringReader sr = new StringReader(request);
-        BufferedReader br = new BufferedReader(sr);
-        List<String> requestStrings = List.of(request.split("\r\n"));
-
-        // when
-        HttpRequest httpRequest = new HttpRequest(br);
-
-        // then
-        assertThat(httpRequest.getHttpRequestLine().toString()).isEqualTo(requestStrings.get(0));
-
-        AtomicInteger index = new AtomicInteger();
-        httpRequest.getHeadersList().forEach(header ->
-                        assertThat(header.toString()).isEqualTo(requestStrings.get(index.getAndIncrement()+1)));
-    }
-
-    private static Stream<String> getHttpRequestString() {
-        return Stream.of(
-                "GET /index.html HTTP/1.1\r\n" +
-                        "Host: localhost:8080\r\n" +
-                        "Connection: keep-alive\r\n" +
-                        "Accept: */*\r\n\r\n",
-                "GET /user/login.html HTTP/1.1\r\n" +
-                        "Host: localhost:8080\r\n" +
-                        "Connection: keep-alive\r\n" +
-                        "Accept: */*\r\n\r\n",
-                "GET /user/form.html HTTP/1.1\r\n" +
-                        "Host: localhost:8080\r\n" +
-                        "Connection: keep-alive\r\n" +
-                        "Accept: */*\r\n\r\n"
-        );
-    }
 
     @ParameterizedTest
     @MethodSource("getSignUpHttpRequestString")
@@ -72,7 +36,7 @@ class UserServiceTest {
         String userId = userInfo.get("userId");
 
         // when
-        HttpResponse httpResponse = UserService.signUp(httpRequest);
+        HttpResponse httpResponse = RequestController.getResponse(httpRequest);
 
         // then
         assertThat(DataBase.findUserById(userId)).isInstanceOf(User.class);
@@ -83,13 +47,13 @@ class UserServiceTest {
 
     private static Stream<String> getSignUpHttpRequestString() {
         return Stream.of(
-                "POST /user/login HTTP/1.1\r\n" +
+                "POST /user/create HTTP/1.1\r\n" +
                         "Host: localhost:8080\r\n" +
                         "Connection: keep-alive\r\n" +
                         "Content-Length: 72\r\n" +
                         "Accept: */*\r\n\r\n"+
                         "userId=grimm&password=12341234&name=name&email=grimm.lee@kakaocorp.com\r\n",
-                "POST /user/login HTTP/1.1\r\n" +
+                "POST /user/create HTTP/1.1\r\n" +
                         "Host: localhost:8080\r\n" +
                         "Connection: keep-alive\r\n" +
                         "Content-Length: 72\r\n" +
