e5fa146697532810e7c3c3dbcaff30026994759e
flip1945 <coghdus1016@naver.com>
2022-02-08 17:26:36 +0900

    refactor: 코드 리팩토링

--- src/main/java/domain/HttpResponse.java
+++ src/main/java/domain/HttpResponse.java
@@ -6,18 +6,18 @@ import java.io.DataOutputStream;
 
 public class HttpResponse {
 
-    public void response200(DataOutputStream dos, byte[] body, ContentType contentType) {
+    public static void response200(DataOutputStream dos, byte[] body, ContentType contentType) {
         ResponseHandler responseHandler = new ResponseHandler(dos);
         responseHandler.response200Header(body.length, contentType.getContentType());
         responseHandler.responseBody(body);
     }
 
-    public void response302(DataOutputStream dos, String location) {
+    public static void response302(DataOutputStream dos, String location) {
         ResponseHandler responseHandler = new ResponseHandler(dos);
         responseHandler.response302Header(location);
     }
 
-    public void response302(DataOutputStream dos, String location, String cookie) {
+    public static void response302(DataOutputStream dos, String location, String cookie) {
         ResponseHandler responseHandler = new ResponseHandler(dos);
         responseHandler.response302Header(location, cookie);
     }
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,18 +2,12 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 import java.util.Map;
 
 import controller.Controller;
-import db.DataBase;
 import domain.*;
-import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import util.HttpRequestUtils;
-import util.IOUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -30,10 +24,8 @@ public class RequestHandler extends Thread {
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
 
-        try (InputStream in = connection.getInputStream();
-             OutputStream out = connection.getOutputStream();
-             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
-             DataOutputStream dos = new DataOutputStream(out)) {
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+             DataOutputStream dos = new DataOutputStream(connection.getOutputStream())) {
 
             RequestLine requestLine = new RequestLine(bufferedReader.readLine());
             log.info("HTTP Request Line : {}", requestLine);
