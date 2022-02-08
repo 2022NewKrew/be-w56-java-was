440dcf14a737e93c62b3f8bbb4d95485c75296ff
carrot.ez <kakaocommerce@kakaocommerceui-MacBookPro.local>
2022-02-08 21:34:15 +0900

    http request 한글 깨짐 문제 해결

--- src/main/java/util/IOUtils.java
+++ src/main/java/util/IOUtils.java
@@ -2,6 +2,11 @@ package util;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 public class IOUtils {
     /**
@@ -13,8 +18,9 @@ public class IOUtils {
      * @throws IOException
      */
     public static String readData(BufferedReader br, int contentLength) throws IOException {
-        char[] body = new char[contentLength];
-        br.read(body, 0, contentLength);
-        return String.copyValueOf(body);
+        char[] charBuf = new char[contentLength];
+        br.read(charBuf, 0, contentLength);
+        String body = new String(charBuf);
+        return URLDecoder.decode(body, StandardCharsets.UTF_8);
     }
 }
