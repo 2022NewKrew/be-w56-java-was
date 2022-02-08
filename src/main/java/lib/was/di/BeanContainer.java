105ae0cfebc9904a6e03d3f405b7a43e0ebd7598
Wooseop Kim <wooseop.kim.dev@gmail.com>
2022-02-08 12:17:11 +0900

    feat(BeanContainer): 등록된 Bean이 없을 경우 기본 생성자 사용

--- src/main/java/lib/was/di/BeanContainer.java
+++ src/main/java/lib/was/di/BeanContainer.java
@@ -2,6 +2,7 @@ package lib.was.di;
 
 import com.google.common.primitives.Primitives;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -21,6 +22,9 @@ public class BeanContainer {
 
     public Object getFirst(Class<?> x) {
         Class<?> wrapped = Primitives.wrap(x);
+        if (!beans.containsKey(wrapped)) {
+            putDefaultConstructor(wrapped);
+        }
         return Optional.ofNullable(beans.get(wrapped))
                 .map(list -> list.get(0))
                 .map(this::instantiate)
@@ -28,10 +32,23 @@ public class BeanContainer {
     }
 
     public List<Object> getAll(Class<?> x) {
-        return beans.getOrDefault(x, Collections.emptyList())
+        Class<?> wrapped = Primitives.wrap(x);
+        if (!beans.containsKey(wrapped)) {
+            putDefaultConstructor(wrapped);
+        }
+        return beans.getOrDefault(wrapped, Collections.emptyList())
                 .stream()
                 .map(this::instantiate)
                 .collect(Collectors.toList());
+    }
+
+    private void putDefaultConstructor(Class<?> x) {
+        try {
+            Constructor<?> constructor = x.getDeclaredConstructor();
+            put(x, new ClassInstantiator(constructor));
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException("No default constructor found for " + x.getName(), e);
+        }
     }
 
     private Object instantiate(Instantiator instantiator) {
