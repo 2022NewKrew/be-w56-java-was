6e7de1a06912205c45dd53002dfd4fac34b54755
MELT <melt.ep00@gmail.com>
2022-02-08 21:42:14 +0900

    간단 웹 서버 6단계 구현 - `Article` 클래스 추가

---
+++ src/main/java/model/Article.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.time.LocalDateTime;
+
+/**
+ * Created by melodist
+ * Date: 2022-02-08 008
+ * Time: 오후 7:47
+ */
+public class Article {
+
+    private User user;
+    private LocalDateTime createdDate;
+    private String content;
+}
