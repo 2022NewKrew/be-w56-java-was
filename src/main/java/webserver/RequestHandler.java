9fb3f19a673af0bac1b4b9297f12482eb8f1316f
cih468 <cih468@naver,com>
2022-02-08 19:52:53 +0900

    response200Header 와 responseBody 함수를 실행하는 함수 response200 생성.

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -9,8 +9,6 @@ import org.slf4j.LoggerFactory;
 import util.HttpRequestUtils;
 import util.IOUtils;
 
-import org.apache.tika.Tika;
-
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
@@ -32,16 +30,19 @@ public class RequestHandler extends Thread {
             String url = HttpRequestUtils.getUrlPath(httpRequestHeader);
             log.info("url Path : {}", url);
 
-            DataOutputStream dos = new DataOutputStream(out);
-            File file = new File("./webapp" + url);
-            String contentType = new Tika().detect(file);
-            log.info("contentType : {}", contentType);
-            byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
-            response200Header(dos, contentType, body.length);
-            responseBody(dos, body);
+            response200(new DataOutputStream(out),url);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private void response200(DataOutputStream dos,String url) throws IOException{
+        String contentType = IOUtils.getContentType(new File("./webapp" + url));
+        log.info("contentType : {}", contentType);
+
+        byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+        response200Header(dos, contentType, body.length);
+        responseBody(dos, body);
     }
 
     private void response200Header(DataOutputStream dos, String contentType, int lengthOfBodyContent) {
