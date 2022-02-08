2be09e276e67f62e0775b5e82e9728c6478fd69c
edward.baek <edward.baek@kakaocorp.com>
2022-02-08 20:35:42 +0900

    feat : HttpRequest를 처리해서 Response를 받아오는 클래스 작성

---
+++ src/main/java/webserver/request/process/ControllerMethodMatcher.java
@@ -0,0 +1,31 @@
+package webserver.request.process;
+
+import webserver.http.RequestLine;
+import webserver.http.Response;
+import webserver.http.request.HttpRequest;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Objects;
+
+public class ControllerMethodMatcher {
+
+    public static <T> Response match (Class<T> clazz, HttpRequest httpRequest)
+            throws IllegalAccessException, InvocationTargetException, NoSuchMethodException, InstantiationException{
+
+        String path = httpRequest.getHeaderAttribute(RequestLine.PATH.name());
+
+        T instance = clazz.getDeclaredConstructor().newInstance();
+        Method[] methods = instance.getClass().getMethods();
+
+        Response result = null;
+        for(Method method : methods){
+            result = MethodProcessor.process(path, httpRequest, instance, result, method);
+        }
+
+        if(Objects.isNull(result)) {
+            return new Response(path, 200);
+        }
+        return result;
+    }
+}
---
+++ src/main/java/webserver/request/process/RequestBodyProcessor.java
@@ -0,0 +1,44 @@
+package webserver.request.process;
+
+import annotation.RequestBody;
+import webserver.http.request.HttpRequest;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Parameter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RequestBodyProcessor {
+
+    public static Object process(HttpRequest httpRequest, Parameter parameter)
+            throws InvocationTargetException, InstantiationException, IllegalAccessException {
+
+        if(parameter.getAnnotation(RequestBody.class) == null) {
+            return null;
+        }
+        for(Constructor<?> constructor : parameter.getType().getDeclaredConstructors()) {
+            return findConstructor(constructor, parameter, httpRequest);
+        }
+        return null;
+    }
+
+    private static Object findConstructor(Constructor<?> constructor, Parameter parameter, HttpRequest httpRequest)
+            throws InvocationTargetException, InstantiationException, IllegalAccessException {
+
+        if (constructor.getParameters().length == httpRequest.bodyValueCount()) {
+            return requestBodyInit(constructor, parameter, httpRequest);
+        }
+        return null;
+    }
+
+    private static Object requestBodyInit(Constructor<?> constructor, Parameter parameter, HttpRequest httpRequest)
+            throws InvocationTargetException, InstantiationException, IllegalAccessException {
+
+        List<Object> objects = new ArrayList<>();
+        for (String name : parameter.getAnnotation(RequestBody.class).names()) {
+            objects.add(httpRequest.getBodyAttribute(name));
+        }
+        return constructor.newInstance(objects.toArray());
+    }
+}
