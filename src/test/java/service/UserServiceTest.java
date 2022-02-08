9ce3926b095344db7b85e318b22b6e44399da7ca
yuchanleeme <yuchanlee.me@gmail.com>
2022-02-08 16:36:39 +0900

    test: 유저 로그인 테스트 코드 작성
    - 제대로된 아이디와 비밀번호를 입력했으면 로그인 되어야 한다. -> index.html로 리다이렉트
    - 아이디와 비밀번호 잘못 입력시 로그인 실패 페이지로 이동한다.
    - 로그인 된 유저의 Cookie 값은 logined=true 이다.
    - 로그인 실패한 유저의 Cookie값은 logined=false 이다.

--- src/test/java/service/UserServiceTest.java
+++ src/test/java/service/UserServiceTest.java
@@ -24,6 +24,18 @@ import static org.assertj.core.api.Assertions.*;
 @DisplayName("유저 관련 테스트")
 class UserServiceTest {
 
+    @BeforeAll
+    public static void beforeAll(){
+        dummyUsers().forEach(DataBase::addUser);
+    }
+
+    private static List<User> dummyUsers(){
+        return List.of(
+                new User("testUser", "12341234", "test", "test@test.com"),
+                new User("testUser2", "12341234", "test2", "test2@test.com")
+        );
+    }
+
     @ParameterizedTest
     @MethodSource("getSignUpHttpRequestString")
     @DisplayName("입력받은 정보로 회원가입이 가능해야 한다.")
@@ -62,4 +74,77 @@ class UserServiceTest {
         );
     }
 
+    @ParameterizedTest
+    @MethodSource("getOKLoginHttpRequestString")
+    @DisplayName("제대로된 아이디와 비밀번호를 입력했으면 로그인 되어야 한다.")
+    public void 아이디_비밀번호_정상입력시_로그인(String request) throws IOException {
+        // given
+        StringReader sr = new StringReader(request);
+        BufferedReader br = new BufferedReader(sr);
+        HttpRequest httpRequest = new HttpRequest(br);
+
+        // when
+        HttpResponse httpResponse = RequestController.getResponse(httpRequest);
+
+        // then
+        assertThat(httpResponse.getStatus()).isEqualTo(HttpStatus.REDIRECT);
+        assertThat(httpResponse.getHeaders().getHeaderByKey("Set-Cookie"))
+                .isEqualTo("Set-Cookie: logined=true; Path=/");
+        assertThat(httpResponse.getHeaders().getHeaderByKey("Location"))
+                .isEqualTo("Location: http://localhost:8080/index.html");
+    }
+
+    @ParameterizedTest
+    @MethodSource("getFailLoginHttpRequestString")
+    @DisplayName("아이디와 비밀번호 잘못 입력시 로그인 실패 페이지로 이동한다.")
+    public void 아이디_비밀번호_잘못입력시_로그인실패(String request) throws IOException {
+        // given
+        StringReader sr = new StringReader(request);
+        BufferedReader br = new BufferedReader(sr);
+        HttpRequest httpRequest = new HttpRequest(br);
+
+        // when
+        HttpResponse httpResponse = RequestController.getResponse(httpRequest);
+
+        // then
+        assertThat(httpResponse.getStatus()).isEqualTo(HttpStatus.REDIRECT);
+        assertThat(httpResponse.getHeaders().getHeaderByKey("Set-Cookie"))
+                .isEqualTo("Set-Cookie: logined=false; Path=/");
+        assertThat(httpResponse.getHeaders().getHeaderByKey("Location"))
+                .isEqualTo("Location: http://localhost:8080/user/login_failed.html");
+    }
+
+    private static Stream<String> getOKLoginHttpRequestString() {
+        return Stream.of(
+                "POST /user/login HTTP/1.1\r\n" +
+                        "Host: localhost:8080\r\n" +
+                        "Connection: keep-alive\r\n" +
+                        "Content-Length: 33\r\n" +
+                        "Accept: */*\r\n\r\n"+
+                        "userId=testUser&password=12341234\r\n",
+                "POST /user/login HTTP/1.1\r\n" +
+                        "Host: localhost:8080\r\n" +
+                        "Connection: keep-alive\r\n" +
+                        "Content-Length: 34\r\n" +
+                        "Accept: */*\r\n\r\n"+
+                        "userId=testUser2&password=12341234\r\n"
+        );
+    }
+
+    private static Stream<String> getFailLoginHttpRequestString() {
+        return Stream.of(
+                "POST /user/login HTTP/1.1\r\n" +
+                        "Host: localhost:8080\r\n" +
+                        "Connection: keep-alive\r\n" +
+                        "Content-Length: 33\r\n" +
+                        "Accept: */*\r\n\r\n"+
+                        "userId=testUser&password=1234\r\n",
+                "POST /user/login HTTP/1.1\r\n" +
+                        "Host: localhost:8080\r\n" +
+                        "Connection: keep-alive\r\n" +
+                        "Content-Length: 34\r\n" +
+                        "Accept: */*\r\n\r\n"+
+                        "userId=testUser2&password=1234\r\n"
+        );
+    }
 }
