6bba630740c1bd9451749f60179fe6518d69f452
chiveloper <wodlr2007@naver.com>
2022-02-08 23:03:06 +0900

    [feature] Status code 추가
    - Unauthorized를 추가함 ( 401 코드 )

--- src/main/java/network/Status.java
+++ src/main/java/network/Status.java
@@ -1,9 +1,12 @@
 package network;
 
 public enum Status {
-    OK(200), FOUND(302), NOTFOUND(404);
+    OK(200),
+    FOUND(302),
+    UNAUTHORIZED(401), NOTFOUND(404);
 
     private int value;
+
     Status(int value) {
         this.value = value;
     }
