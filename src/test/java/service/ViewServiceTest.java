26630f97f299b6b75106456124278d56d23e1206
yuchanleeme <yuchanlee.me@gmail.com>
2022-02-08 17:18:02 +0900

    test: 로그인 된 유저 테스트
    - 로그인 된 유저가 로그인 url 접근 시 index 페이지로 이동시킨다.

--- src/test/java/service/ViewServiceTest.java
+++ src/test/java/service/ViewServiceTest.java
@@ -1,9 +1,13 @@
 package service;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+import web.controller.RequestController;
 import web.http.request.HttpRequest;
+import web.http.response.HttpResponse;
+import web.http.response.HttpStatus;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -53,4 +57,28 @@ public class ViewServiceTest {
                         "Accept: */*\r\n\r\n"
         );
     }
+
+    @Test
+    @DisplayName("로그인 된 유저라면 로그인 url 접근 시 index 페이지로 이동한다.")
+    public void HTTP_요청_확인_테스트() throws IOException {
+        // given
+        String request = "GET /user/login.html HTTP/1.1\r\n" +
+                "Host: localhost:8080\r\n" +
+                "Connection: keep-alive\r\n" +
+                "Cookie: logined=true; Path=/\r\n" +
+                "Accept: */*\r\n\r\n";
+        StringReader sr = new StringReader(request);
+        BufferedReader br = new BufferedReader(sr);
+        HttpRequest httpRequest = new HttpRequest(br);
+
+        // when
+        HttpResponse httpResponse = RequestController.getResponse(httpRequest);
+
+        // then
+        assertThat(httpResponse.getStatus()).isEqualTo(HttpStatus.REDIRECT);
+        assertThat(httpResponse.getHeaders().getHeaderByKey("Location"))
+                .isEqualTo("Location: http://localhost:8080/index.html");
+    }
+
+
 }
