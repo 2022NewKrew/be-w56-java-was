3a6de64f57742f9ecf464300a8840a6326c9d51c
chiveloper <wodlr2007@naver.com>
2022-02-09 00:50:55 +0900

    [feature] 로그인 구현
    - 로그인 구현
    - Controller, Service, user에 로그인 기능 구현

--- src/main/java/webserver/controller/PostController.java
+++ src/main/java/webserver/controller/PostController.java
@@ -1,8 +1,8 @@
 package webserver.controller;
 
-import domain.User;
 import network.HttpRequest;
 import network.HttpResponse;
+import network.Status;
 import webserver.service.UserService;
 
 import java.io.OutputStream;
@@ -12,16 +12,24 @@ public class PostController {
     private UserService userService;
     private OutputStream outputStream;
 
-    public PostController(HttpRequest httpRequest, OutputStream outputStream){
+    public PostController(HttpRequest httpRequest, OutputStream outputStream) {
         this.outputStream = outputStream;
         this.userService = new UserService(httpRequest);
     }
 
     public void handlePost(String path) {
-        switch (path){
+        switch (path) {
             case "/user/create":
                 userService.signUp();
                 HttpResponse.redirect("/index.html", outputStream);
+                break;
+            case "/user/login":
+                Status status = userService.logIn();
+                if (status == Status.OK) {
+                    HttpResponse.redirect("/index.html", outputStream);
+                    break;
+                }
+                HttpResponse.redirect("/user/login_failed.html", outputStream);
                 break;
         }
     }
