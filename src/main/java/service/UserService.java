6c5722f0b1dc25def09187566adcdbe6fd508faf
yoonmyunghoon <52689727+yoonmyunghoon@users.noreply.github.com>
2022-02-10 04:02:02 +0900

    웹 서버 구현 7 단계 일부 구현 (#431)
    * h2 database config 설정
    
    - connection 생성
    
    * h2 database 연결 및 로직 처리
    
    - h2 connection 사용하는 InMemoryUserRepository 생성
    - UserService, ViewService 등 User 관련 DB 로직을 InMemoryUserRepository 사용하도록 변경
    
    Co-authored-by: reynold-yoon <reynold.yoon@kakaocorp.com>

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,29 +1,28 @@
 package service;
 
-import db.DataBase;
 import http.Request;
 import http.RequestHeader;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import repository.InMemoryUserRepository;
 
 import java.util.Objects;
 
 public class UserService {
     private static final Logger log = LoggerFactory.getLogger(UserService.class);
 
+    private static final InMemoryUserRepository userRepository = InMemoryUserRepository.getInstance();
+
     public static void join(User user) {
-        DataBase.addUser(user);
-        log.debug("User DataBase Status: {}", DataBase.findAll());
+        userRepository.addUser(user);
+        log.debug("User DataBase Status: {}", userRepository.findAll());
     }
 
     public static boolean login(String userId, String password) {
-        log.debug("User DataBase Status: {}", DataBase.findAll());
+        log.debug("User DataBase Status: {}", userRepository.findAll());
         log.debug("userId: {}, password: {}", userId, password);
-        User user = DataBase.findUserById(userId);
-        if (user == null) {
-            return false;
-        }
+        User user = userRepository.findUserById(userId).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 유저입니다."));
         return Objects.equals(user.getPassword(), password);
     }
 
