f98543d4bf2a0ac1957caecd9426dce58e9ad55e
jaden <jaden.dev@kakaocorp.com>
2022-02-09 13:41:26 +0900

    user 리스트 추가 부분을 별개 메서드로 분리

--- src/main/java/util/DynamicHtmlBuilder.java
+++ src/main/java/util/DynamicHtmlBuilder.java
@@ -5,7 +5,7 @@ import model.User;
 import java.util.Collection;
 
 public class DynamicHtmlBuilder {
-    public static void build(String path, Collection<User> collection) {
+    public static void build(String path, Collection collection) {
         String baseHtml = IOUtils.readHtml(path);
         String startRegex = "<tbody>";
         String endRegex = "</tbody>";
@@ -15,20 +15,26 @@ public class DynamicHtmlBuilder {
         StringBuilder contentsBuilder = new StringBuilder();
         contentsBuilder.append(baseHtml, 0, startIndex);
         contentsBuilder.append("\n");
-        int idx = 1;
-        for (User user : collection) {
-            contentsBuilder.append("<tr>\n");
-            contentsBuilder.append("<th scope=\"row\">" + Integer.toString(idx++) + "</th> ");
-            contentsBuilder.append("<td>" + user.getUserId() + "</td> ");
-            contentsBuilder.append("<td>" + user.getName() + "</td> ");
-            contentsBuilder.append("<td>" + user.getEmail() + "</td> ");
-            contentsBuilder.append("<td><a href=\"#\" class=\"btn btn-success\" role=\"button\">수정</a></td>\n");
-            contentsBuilder.append("</tr>\n");
-        }
+        contentsBuilder.append(userListHtml(collection));
 
         contentsBuilder.append(baseHtml.substring(endIndex));
 
         IOUtils.writeFile("./webapp/user/dynamicList.html", contentsBuilder.toString());
 
+    }
+
+    private static String userListHtml(Collection<User> collection) {
+        StringBuilder userList = new StringBuilder();
+        int idx = 1;
+        for (User user : collection) {
+            userList.append("<tr>\n");
+            userList.append("<th scope=\"row\">" + Integer.toString(idx++) + "</th> ");
+            userList.append("<td>" + user.getUserId() + "</td> ");
+            userList.append("<td>" + user.getName() + "</td> ");
+            userList.append("<td>" + user.getEmail() + "</td> ");
+            userList.append("<td><a href=\"#\" class=\"btn btn-success\" role=\"button\">수정</a></td>\n");
+            userList.append("</tr>\n");
+        }
+        return userList.toString();
     }
 }
