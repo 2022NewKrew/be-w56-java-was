0d4be688f4d810ad82815371fe60bb132e1b64e9
SeongMin Park <parkseongmin98@gmail.com>
2022-02-09 15:53:27 +0900

    test: User 테스트 추가

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -23,10 +23,6 @@ public class User {
         return userId;
     }
 
-    public String getPassword() {
-        return password;
-    }
-
     public String getName() {
         return name;
     }
