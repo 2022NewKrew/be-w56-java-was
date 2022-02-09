2fe116d9568e393f914fa62cd2acced60cca410d
Heejoo Shin <48991173+ev-moon@users.noreply.github.com>
2022-02-09 22:04:54 +0900

    feat: 웹 서버 5단계 구현 (#405)

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -3,6 +3,8 @@ package service;
 import db.DataBase;
 import model.User;
 
+import java.util.Iterator;
+
 
 public enum UserService {
     INSTANCE;
@@ -14,5 +16,19 @@ public enum UserService {
     public boolean userLogin(String userId, String password) {
         User user = DataBase.findUserById(userId);
         return user != null && user.getPassword().equals(password);
+    }
+
+    public String getUserList() {
+        StringBuilder sb = new StringBuilder();
+        Iterator<User> userIterator = DataBase.findAll().iterator();
+        for (int idx = 1; userIterator.hasNext(); idx++) {
+            User user = userIterator.next();
+            sb.append("<tr><th scope=\"row\">" + idx + "</th>" +
+                    "<td>" + user.getUserId() + "</td>" +
+                    "<td>" + user.getName() + "</td>" +
+                    "<td>" + user.getEmail() + "</td>" +
+                    "<td><a href=\"#\" class=\"btn btn-success\" role=\"button\">수정</a></td></tr>");
+        }
+        return sb.toString();
     }
 }
--- src/main/java/webserver/Controller.java
+++ src/main/java/webserver/Controller.java
@@ -6,14 +6,19 @@ import util.HttpRequestUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Controller {
     public static final Controller INSTANCE = new Controller();
 
     private final UserService userService;
+    private final List<String> RESTRICTED_PAGES = Arrays.asList("/user/list.html");
 
     private Controller() {
         this.userService = UserService.INSTANCE;
@@ -22,7 +27,13 @@ public class Controller {
     public byte[] getRequest(Map<String, String> requestInfo) {
         byte[] body;
         try {
-            body = Files.readAllBytes(new File("./webapp" + requestInfo.get("target")).toPath());
+            String targetResource = requestInfo.get("target");
+            boolean authorized = checkAuthorization(requestInfo);
+            if (RESTRICTED_PAGES.contains(targetResource) && !authorized)
+                targetResource = "/user/login.html";
+            if (targetResource.equals("/user/list.html"))
+                return getUserList();
+            body = Files.readAllBytes(new File("./webapp" + targetResource).toPath());
         } catch (IOException e) {
             body = "Hello World".getBytes();
         }
@@ -56,5 +67,18 @@ public class Controller {
         postHeaderInfo.put("cookie", "logined=" + loginSuccess + "; Path=/");
 
         return postHeaderInfo;
+    }
+
+    private boolean checkAuthorization(Map<String, String> requestInfo) {
+        Map<String, String> cookieMap = HttpRequestUtils.parseCookies(requestInfo.getOrDefault("Cookie", ""));
+        return Boolean.parseBoolean(cookieMap.getOrDefault("logined", "false"));
+    }
+
+    private byte[] getUserList() throws IOException {
+        String[] tokens = (Files.readString(Path.of("./webapp/user/list.html"))).split("</?tbody>");
+        StringBuilder sb = new StringBuilder(tokens[0] + "<tbody>");
+        sb.append(userService.getUserList());
+        sb.append("</tbody>").append(tokens[2]);
+        return sb.toString().getBytes(StandardCharsets.UTF_8);
     }
 }
