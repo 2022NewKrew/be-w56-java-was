b02ac29ee2d56a45adc54b1d12fef1954d7fbb1a
jaden <jaden.dev@kakaocorp.com>
2022-02-09 15:23:37 +0900

    로그인 페이지 리다이렉션 주소 수정

--- src/main/java/Controller/HttpController.java
+++ src/main/java/Controller/HttpController.java
@@ -22,6 +22,12 @@ public class HttpController {
         // PostMapping("/user/login") 와 동일
         else if (httpRequest.getMethod().equals(HttpMethod.POST) && httpRequest.getUrl().equals("/user/login")) {
             boolean validLogin = httpService.validLogin(httpRequest.getBody().get("userId"), httpRequest.getBody().get("password"));
+            if (validLogin) {
+                return "redirect:/index.html";
+            }
+            else {
+                return "redirect:/user/login_failed.html";
+            }
         }
         // GetMapping("/user/list") 와 동일
         else if (httpRequest.getMethod().equals(HttpMethod.GET) && httpRequest.getUrl().equals("/user/list")) {
