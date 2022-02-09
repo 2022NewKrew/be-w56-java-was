059b80d2be287b338814d210aef93b52cf06a95d
yuchanleeme <yuchanlee.me@gmail.com>
2022-02-09 19:29:14 +0900

    feat: MySQL JDBC 연동
    - Database 클래스에서 connect, close

--- src/main/java/db/DataBase.java
+++ src/main/java/db/DataBase.java
@@ -1,14 +1,37 @@
 package db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Collection;
 import java.util.Map;
+import java.util.Properties;
 
 import com.google.common.collect.Maps;
 
 import model.User;
+import util.PropertiesUtils;
 
 public class DataBase {
-    private static Map<String, User> users = Maps.newHashMap();
+    private static Connection connection;
+    private static final Map<String, User> users = Maps.newHashMap();
+
+    public static void connect(){
+        Properties prop = PropertiesUtils.readDBProperties();
+        try{
+            assert prop != null;
+            String url = prop.getProperty("url");
+            String userName = prop.getProperty("userName");
+            String password = prop.getProperty("password");
+            connection = DriverManager.getConnection(url, userName, password);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void close() throws SQLException {
+        connection.close();
+    }
 
     public static void addUser(User user) {
         users.put(user.getUserId(), user);
