e6509aa38fd5818c59d139dbcca54495c459e6bd
chiveloper <wodlr2007@naver.com>
2022-02-09 18:52:17 +0900

    [refactor] 줄 맞추기

--- src/main/java/webserver/service/UserService.java
+++ src/main/java/webserver/service/UserService.java
@@ -3,7 +3,6 @@ package webserver.service;
 import domain.User;
 import domain.UserList;
 import network.HttpRequest;
-import network.Status;
 
 import java.util.Map;
 
@@ -15,28 +14,21 @@ public class UserService {
         this.httpRequest = httpRequest;
     }
 
-    public Status signUp() {
+    public void signUp() {
 
         Map<String, String> body = httpRequest.getBody();
         if (!body.isEmpty()) {
             User user = new User(body);
             UserList.append(user.getUserId(), user);
-            return Status.FOUND;
         }
-        return Status.NOTFOUND;
     }
 
-    public Status logIn() {
+    public Boolean logIn() {
         Map<String, String> body = httpRequest.getBody();
         String inputId = body.get("userId");
         String inputPw = body.get("password");
 
-        if ( isValidUser(inputId, inputPw) ){
-            User user = UserList.get(inputId);
-            System.out.println(httpRequest);
-            return Status.OK;
-        }
-        return Status.FOUND;
+        return isValidUser(inputId, inputPw);
     }
 
     private boolean isValidUser(String inputId, String inputPw){
@@ -44,7 +36,4 @@ public class UserService {
         return user != null && user.getPassword().equals(inputPw);
     }
 
-    public Status home() {
-        return Status.OK;
-    }
 }
