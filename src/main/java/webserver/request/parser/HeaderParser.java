f65eb1189653c5d869b2d1c52b99bc403173af6b
edward.baek <edward.baek@kakaocorp.com>
2022-02-09 23:03:59 +0900

    refactor : headerLineParser를 HeaderLine에 포함시킴

--- src/main/java/webserver/request/parser/HeaderParser.java
+++ src/main/java/webserver/request/parser/HeaderParser.java
@@ -5,6 +5,7 @@ import webserver.http.request.HttpHeader;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 public class HeaderParser {
@@ -14,7 +15,7 @@ public class HeaderParser {
     }
 
     public static HttpHeader parse(BufferedReader br) throws IOException {
-        Map<String, String> headerMap = HeaderLineParser.parse(br);
+        Map<String, String> headerMap = headerLineParse(br);
         String headerLine;
         while (!(headerLine = br.readLine()).equals("")) {
             HttpRequestUtils.Pair pair = HttpRequestUtils.parseHeader(headerLine);
@@ -22,6 +23,15 @@ public class HeaderParser {
         }
         Map<String, String> cookieMap = cookieParse(headerMap.get("Cookie"));
         return new HttpHeader(headerMap, cookieMap);
+    }
+
+    private static Map<String, String> headerLineParse(BufferedReader br) throws IOException {
+        String httpRequestLine = br.readLine();
+        Map<String, String> headerMap = new HashMap<>();
+        for (HttpRequestUtils.Pair parsedRequestLine : HttpRequestUtils.parseRequestLine(httpRequestLine)) {
+            headerMap.put(parsedRequestLine.getKey(), parsedRequestLine.getValue());
+        }
+        return headerMap;
     }
 
     private static Map<String, String> cookieParse(String cookieLine) {
