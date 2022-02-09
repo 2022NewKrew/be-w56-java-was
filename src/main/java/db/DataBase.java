344225b6eeefa29ab8415c141d0d8b92ab81bc31
yuchanleeme <yuchanlee.me@gmail.com>
2022-02-09 20:02:10 +0900

    feat: 회원 정보 저장 및 조회 기능 구현
    - jdbc를 이용해서 MySQL에 저장 및 조회

--- src/main/java/db/DataBase.java
+++ src/main/java/db/DataBase.java
@@ -1,11 +1,7 @@
 package db;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.Collection;
-import java.util.Map;
-import java.util.Properties;
+import java.sql.*;
+import java.util.*;
 
 import com.google.common.collect.Maps;
 
@@ -14,7 +10,6 @@ import util.PropertiesUtils;
 
 public class DataBase {
     private static Connection connection;
-    private static final Map<String, User> users = Maps.newHashMap();
 
     public static void connect(){
         Properties prop = PropertiesUtils.readDBProperties();
@@ -34,14 +29,53 @@ public class DataBase {
     }
 
     public static void addUser(User user) {
-        users.put(user.getUserId(), user);
+        String sql = "INSERT INTO USERS(userId, password, name, email) VALUES (?, ?, ?, ?)";
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, user.getUserId());
+            statement.setString(2, user.getPassword());
+            statement.setString(3, user.getName());
+            statement.setString(4, user.getEmail());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public static User findUserById(String userId) {
-        return users.get(userId);
+        String sql = "SELECT * FROM USERS WHERE userId=?";
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, userId);
+
+            ResultSet resultSet = statement.executeQuery();
+            resultSet.next();
+
+            User user = new User(resultSet.getString("userId"),
+                    resultSet.getString("password"),
+                    resultSet.getString("name"),
+                    resultSet.getString("email"));
+            resultSet.close();
+            return user;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
-    public static Collection<User> findAll() {
-        return users.values();
+    public static List<User> findAll() {
+        List<User> userList = new ArrayList<>();
+        String sql = "SELECT * FROM USERS";
+        try (Statement statement = connection.createStatement(); ResultSet resultSet = statement.executeQuery(sql)) {
+            while (resultSet.next()) {
+                userList.add(new User(
+                        resultSet.getString("userId"),
+                        resultSet.getString("password"),
+                        resultSet.getString("name"),
+                        resultSet.getString("email")
+                ));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return userList;
     }
 }
