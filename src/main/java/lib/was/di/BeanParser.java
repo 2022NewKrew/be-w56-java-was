349e9966c8f3bd4a36e051a3c9d0459705570d53
Wooseop Kim <wooseop.kim.dev@gmail.com>
2022-02-09 10:54:56 +0900

    refactor(BeanParser): 메서드 및 클래스 분리/추출

--- src/main/java/lib/was/di/BeanParser.java
+++ src/main/java/lib/was/di/BeanParser.java
@@ -1,12 +1,10 @@
 package lib.was.di;
 
 import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class BeanParser {
 
-    // TODO refactor
     public BeanContainer parse(Class<?>[] classes) {
         BeanContainer container = new BeanContainer();
         for (Class<?> clazz : classes) {
@@ -15,46 +13,44 @@ public class BeanParser {
                 continue;
             }
             container.put(clazz, new ClassInstantiator(constructor));
-            for (Method m : clazz.getDeclaredMethods()) {
-                if (m.getAnnotation(Bean.class) == null) {
-                    continue;
-                }
-                container.put(m.getReturnType(), new Instantiator() {
-                    @Override
-                    public Class<?>[] getParameterTypes() {
-                        return m.getParameterTypes();
-                    }
-
-                    @Override
-                    public Object newInstance(Object[] parameters)
-                            throws IllegalAccessException, InvocationTargetException {
-                        return m.invoke(container.getFirst(clazz), parameters);
-                    }
-                });
-            }
+            putMethodInstantiators(container, clazz);
         }
         return container;
+    }
+
+    private void putMethodInstantiators(BeanContainer container, Class<?> clazz) {
+        for (Method method : clazz.getDeclaredMethods()) {
+            if (!method.isAnnotationPresent(Bean.class)) {
+                continue;
+            }
+            Class<?> returnType = method.getReturnType();
+            Instantiator instantiator = new MethodInstantiator(method, () -> container.getFirst(clazz));
+            container.put(returnType, instantiator);
+        }
     }
 
     private Constructor<?> getInjectableConstructor(Class<?> clazz) {
         if (!clazz.isAnnotationPresent(Bean.class)) {
             return null;
         }
-        Constructor<?> constructor = null;
+        Constructor<?> constructor;
+        try {
+            constructor = clazz.getDeclaredConstructor();
+        } catch (NoSuchMethodException ignored) {
+            constructor = null;
+        }
         Constructor<?>[] constructors = clazz.getDeclaredConstructors();
         for (Constructor<?> c : constructors) {
-            if (c.getParameterTypes().length == 0) {
-                constructor = c;
+            if (!c.isAnnotationPresent(Inject.class)) {
+                continue;
             }
-            Inject annotation = c.getAnnotation(Inject.class);
-            if (annotation != null) {
-                constructor = c;
-                break;
-            }
+            constructor = c;
+            break;
         }
         if (constructor == null) {
             throw new RuntimeException("No matching constructor for class " + clazz.getName());
         }
         return constructor;
     }
+
 }
