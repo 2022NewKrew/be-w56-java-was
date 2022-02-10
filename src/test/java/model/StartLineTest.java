01bbb7797ba28f6c5d023ec2e13c3e5328fac416
SeongMin Park <parkseongmin98@gmail.com>
2022-02-10 14:57:59 +0900

    test: StartLineTest 나머지 함수 테스트 추가

--- src/test/java/model/StartLineTest.java
+++ src/test/java/model/StartLineTest.java
@@ -5,7 +5,9 @@ import static org.junit.jupiter.api.Assertions.*;
 import exceptions.BadRequestFormatException;
 import exceptions.InvalidHttpMethodException;
 import exceptions.InvalidHttpVersionException;
+import exceptions.InvalidQueryFormatException;
 import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
@@ -56,19 +58,61 @@ class StartLineTest {
                 () -> StartLine.of(startLineMessage));
     }
 
-    // TODO - 아래 테스트 추후 구현
     @Test
-    @Disabled
+    @DisplayName("[성공] method를 잘 들고와야 한다")
     void getHttpMethod() {
+        String startLineMessage = HttpMethod.GET.name() + " /index.html HTTP/1.1";
+        StartLine startLine = StartLine.of(startLineMessage);
+
+        HttpMethod methodResult = startLine.getHttpMethod();
+
+        Assertions.assertEquals(methodResult.name(), HttpMethod.GET.name());
     }
 
     @Test
-    @Disabled
+    @DisplayName("[성공] url을 잘 들고와야 한다")
     void getUrl() {
+        String startLineMessage = "GET /index.html HTTP/1.1";
+        StartLine startLine = StartLine.of(startLineMessage);
+
+        String urlResult = startLine.getUrl();
+
+        Assertions.assertNotEquals(urlResult, "/index.htm");
+        Assertions.assertNotEquals(urlResult, "index.html");
+        Assertions.assertEquals(urlResult, "/index.html");
     }
 
     @Test
-    @Disabled
+    @DisplayName("[성공] url을 잘 들고와야 한다 - 쿼리가 있는 경우")
+    void getUrl_By_Query() {
+        String startLineMessage = "GET /user/create?aa=bb HTTP/1.1";
+        StartLine startLine = StartLine.of(startLineMessage);
+
+        String urlResult = startLine.getUrl();
+
+        Assertions.assertNotEquals(urlResult, "user/create");
+        Assertions.assertNotEquals(urlResult, "/user/create?");
+        Assertions.assertEquals(urlResult, "/user/create");
+    }
+
+    @Test
+    @DisplayName("[성공] 쿼리를 잘 들고 와야 한다")
     void getQuery() {
+        String startLineMessage = "GET /user/create?aa=bb&cc=dd HTTP/1.1";
+        Map<String, String> query = Map.of("aa", "bb", "cc", "dd");
+        StartLine startLine = StartLine.of(startLineMessage);
+
+        Map<String, String> queryResult = startLine.getQuery();
+
+        Assertions.assertEquals(queryResult, query);
+    }
+
+    @DisplayName("[실패] 쿼리 형식이 잘못 된 경우")
+    @ParameterizedTest(name = "query = {0}")
+    @ValueSource(strings = {"GET /user/create?aa HTTP/1.1", "GET /user/create?aa=bb&cc HTTP/1.1"})
+    void getQuery_FailedBy_Query(String startLineMessage) {
+
+        Assertions.assertThrows(InvalidQueryFormatException.class,
+                () -> StartLine.of(startLineMessage));
     }
 }
