7233a58fb86e5f151f0a2fe7a3f63f868199501e
kenny.park <kenny.park@kakaocorp.com>
2022-02-13 14:06:37 +0900

    memo 등록시, 쿠키에 로그인 정보가 없으면 로그인 페이지로 redirect하도록 수정

--- src/main/java/webserver/controller/MemoController.java
+++ src/main/java/webserver/controller/MemoController.java
@@ -1,6 +1,7 @@
 package webserver.controller;
 
 import dto.MemoCreateDto;
+import org.apache.commons.lang3.StringUtils;
 import service.MemoService;
 import util.annotation.RequestMapping;
 import webserver.Request;
@@ -19,12 +20,17 @@ public class MemoController {
 
     @RequestMapping(value="/memos", method="POST")
     public ModelAndView create(Request request, Response response) {
-        memoService.create(MemoCreateDto.builder()
-                .userId(Integer.parseInt(request.getCookie("userId")))
-                .content(request.getParameter("content"))
-                .build());
+        if(StringUtils.equals(request.getCookie("logined"), "true")){
+            memoService.create(MemoCreateDto.builder()
+                    .userId(Integer.parseInt(request.getCookie("userId")))
+                    .content(request.getParameter("content"))
+                    .build());
+            ModelAndView mv = new ModelAndView();
+            mv.setViewName("redirect:/index.html");
+            return mv;
+        }
         ModelAndView mv = new ModelAndView();
-        mv.setViewName("redirect:/index.html");
+        mv.setViewName("redirect:/user/login.html");
         return mv;
     }
 
