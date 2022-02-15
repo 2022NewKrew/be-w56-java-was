77151686bfc51705e6cefc5360f43fc7241fbb3c
cih468 <cih468@naver,com>
2022-02-15 14:35:54 +0900

    controller 클래스의 패키지 이동을 인해 import 경로 변경.

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,7 +3,7 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 
-import controller.Controller;
+import webserver.controller.Controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.ControllerHandler;
